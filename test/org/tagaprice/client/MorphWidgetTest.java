79ffd7f6af5d589ff6a16e729af9533038f78fdb
anca_cismasiu@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <anca_cismasiu@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-05-17 18:51:22 +0000

    test folder

---
+++ test/org/tagaprice/client/MorphWidgetTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010 TagAPrice.org
+ * 
+ * Licensed under the Creative Commons License. You may not
+ * use this file except in compliance with the License. 
+ *
+ * http://creativecommons.org/licenses/by-nc/3.0/
+*/
+
+/**
+ * Project: TagAPriceUI
+ * Filename: MorphWidgetTest.java
+ * Date: May 17, 2010
+*/
+package org.tagaprice.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MorphWidgetTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSetText() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
