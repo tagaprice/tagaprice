52e4c45deeded27a02575905939cb34684abcdd2
anca_cismasiu@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <anca_cismasiu@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-05-19 22:42:38 +0000

    SearchWidget and ListWidget with up/down-arrow key select

--- src/org/tagaprice/client/ListWidget.java
+++ src/org/tagaprice/client/ListWidget.java
@@ -5,17 +5,16 @@
  * use this file except in compliance with the License. 
  *
  * http://creativecommons.org/licenses/by-nc/3.0/
-*/
+ */
 
 /**
  * Project: TagAPriceUI
  * Filename: ListWidget.java
  * Date: May 17, 2010
-*/
+ */
 package org.tagaprice.client;
 
 import java.util.ArrayList;
-
 import org.tagaprice.shared.Entity;
 import org.tagaprice.shared.ProductData;
 import org.tagaprice.shared.ShopData;
@@ -25,39 +24,80 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class ListWidget<T extends EntityPreview> extends Composite{
 
 	private VerticalPanel verticalPanel;
-	
+	private int currentSelection=-1;
+
+
 	public ListWidget(){
 		verticalPanel=new VerticalPanel();
 		verticalPanel.setWidth("100%");
 		initWidget(verticalPanel);
+
 	}
-	
+
 	public ListWidget(ArrayList<Entity> entityData){
 		this();
 		populateList(entityData);
 	}
-	
+
 	private void populateList(ArrayList<Entity> entityData){
 		verticalPanel.clear();
-		
+
 		for(Entity e: entityData){
 			verticalPanel.add(createPreview(e));
 		}
-		
+
 	}
-	
+
 	private EntityPreview createPreview(Entity e){
 		EntityPreview rc = null;
-		
+
 		if(e instanceof ShopData){
 			rc= new ShopPreview((ShopData)e, false);
 		}else if(e instanceof ProductData){
 			rc=new ProductPreview((ProductData) e, false);
 		}
-		
+
 		return rc;
 	}
-	
-	
+
+	public void highlightNextSuggestion(){
+		if(currentSelection!=-1)
+			verticalPanel.getWidget(currentSelection).removeStyleName("highlightSuggestion");
+		if(currentSelection<verticalPanel.getWidgetCount()-1)	
+			currentSelection++;
+		verticalPanel.getWidget(currentSelection).addStyleName("highlightSuggestion");
+		
+	}
+
+
+	public void highlightPrevSuggestion(){
+		if(currentSelection!=0){
+			verticalPanel.getWidget(currentSelection).removeStyleName("highlightSuggestion");
+		}
+		if(currentSelection>0){
+			currentSelection--;
+			verticalPanel.getWidget(currentSelection).addStyleName("highlightSuggestion");
+		}
+	}
+
+
+	public Entity getSelection(){
+		Entity rc=null;
+
+		EntityPreview ep = (EntityPreview)verticalPanel.getWidget(currentSelection);
+		if(ep instanceof ProductPreview){
+			rc=((ProductPreview) ep).getProductData();
+		}else if(ep instanceof ShopPreview){
+			rc=((ShopPreview) ep).getshopData();
+		}
+
+		return rc;
+	}
+
+
+
+
+
+
 }
 
--- src/org/tagaprice/client/SearchWidget.java
+++ src/org/tagaprice/client/SearchWidget.java
@@ -6,8 +6,13 @@ import org.tagaprice.shared.Entity;
 import org.tagaprice.shared.TaPManager;
 import org.tagaprice.shared.TaPManagerImpl;
 
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -15,42 +20,49 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 public class SearchWidget extends Composite{
-	
+
 	private TextBox textBox;
 	private VerticalPanel suggestPanel;
 	private PopupPanel suggestPopup = new PopupPanel(true);
 	private ListWidget<EntityPreview> suggestList; 
 	private TaPManager tapManager;
-	
-	
+
+
 	public SearchWidget(){
 		tapManager= TaPManagerImpl.getInstance();
 		suggestPanel = new VerticalPanel();
 		textBox= new TextBox();
 		suggestList = new ListWidget<EntityPreview>();
-		
-		textBox.addKeyDownHandler(new KeyDownHandler() {
-			
+
+		textBox.addKeyPressHandler(new KeyPressHandler() {
+
 			@Override
-			public void onKeyDown(KeyDownEvent event) {
-				//suggestList.removeFromParent();
-				sendSearchRequest(textBox.getText());				
+			public void onKeyPress(KeyPressEvent event) {
+				if(event.getCharCode()==KeyCodes.KEY_DOWN){
+					suggestList.highlightNextSuggestion();
+				} else if(event.getCharCode()==KeyCodes.KEY_UP){
+					suggestList.highlightPrevSuggestion();
+				}  else if(event.getCharCode()==KeyCodes.KEY_ENTER){	
+					//display page for  suggestList.getSelection();
+					
+				}else sendSearchRequest(textBox.getText());
 			}
+
 		});
-		
+
 		suggestPanel.add(textBox);		
 		initWidget(suggestPanel);
-		
+
 		//style
 		textBox.setWidth("100%");
 		suggestPanel.setWidth("100%");
 		suggestPopup.setWidth("400px");
-		
+
 		suggestPopup.setWidget(suggestList);
-		
+
 	}	
-	
-	
+
+
 	public void setSuggestions(ArrayList<Entity> suggestData){
 		suggestList = new ListWidget<EntityPreview>(suggestData);
 		//suggestPopup.showRelativeTo(textBox);
@@ -58,9 +70,9 @@ public class SearchWidget extends Composite{
 		suggestPopup.setWidget(suggestList);
 		suggestPopup.showRelativeTo(suggestPanel);
 	}
-	
+
 	private void sendSearchRequest(String input){
 		tapManager.search(input, this);
 	}
-	
+
 }
