bead5292f85955078c443d1e33cf4eab49f35820
anca_cismasiu@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <anca_cismasiu@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-05-27 14:53:07 +0000

    ...

--- src/org/tagaprice/client/ShopChooser.java
+++ src/org/tagaprice/client/ShopChooser.java
@@ -14,12 +14,28 @@
 */
 package org.tagaprice.client;
 
+import org.tagaprice.client.SearchWidget.Filter;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class ShopChooser extends Composite{
-
+	interface MyUiBinder extends UiBinder<VerticalPanel, ShopChooser>{}
+	MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+	
+	@UiField VerticalPanel verticalPanel;
+	@UiField SearchWidget searchWidget;
+	
+	
 	public ShopChooser(){
-		initWidget(this);
+		verticalPanel = new VerticalPanel();
+		initWidget(verticalPanel);
+		searchWidget = new SearchWidget(Filter.SHOP);
+		verticalPanel.add(searchWidget);
+		
 		
 		
 	}
