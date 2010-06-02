8d490cac974360b43706684107c79ab6ad5e7a35
juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-06-02 07:10:32 +0000

    changed absolut to relativ path in .classpath for the maps.jar

--- src/org/tagaprice/client/ProductPage.java
+++ src/org/tagaprice/client/ProductPage.java
@@ -28,11 +28,8 @@ import org.tagaprice.shared.PropertyList;
 import org.tagaprice.shared.TaPManager;
 import org.tagaprice.shared.TaPManagerImpl;
 import org.tagaprice.shared.Type;
-
-
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
