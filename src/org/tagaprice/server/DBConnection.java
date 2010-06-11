c7ce9917ed8863da391fe2772c4426530f60c495
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-06-11 16:50:19 +0000

    removed old code from DBConnection (which is a class we already used in another university project)

--- src/org/tagaprice/server/DBConnection.java
+++ src/org/tagaprice/server/DBConnection.java
@@ -26,7 +26,6 @@ import org.postgresql.ds.PGConnectionPoolDataSource;
 public class DBConnection {
 	// shared connection pool
 	private static ConnectionPoolDataSource dataSource;
-	private static boolean isTesting = false;
 	private PooledConnection dbConn;
 	
 	/**
@@ -68,97 +67,39 @@ public class DBConnection {
 			ds.setUser(prop.getProperty("user"));
 			ds.setPassword(prop.getProperty("pwd"));
 			dataSource = ds;
-			isTesting = !prop.getProperty("testing").equals("false");
 		}
 	}
 	
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#disconnect()
-	 */
 	public void disconnect() throws SQLException {
 		if (dbConn != null) {
-			if (isTesting) {
-				// rollback transaction
-				Statement s = dbConn.getConnection().createStatement();
-				s.execute("ROLLBACK");
-			}
 			dbConn.close();
 		}
 	}
 	
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#getConnection()
-	 */
 	public Connection getConnection() throws SQLException {
 		if (dbConn == null) {
 			dbConn = dataSource.getPooledConnection();
-			if (isTesting) {
-				// begin transaction
-				Statement s = dbConn.getConnection().createStatement();
-				s.execute("BEGIN");
-			}
 		}
 		return dbConn.getConnection();
 	}
 
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#prepareStatement(java.lang.String)
-	 */
 	public PreparedStatement prepareStatement(String stmt) throws SQLException {
 		return getConnection().prepareStatement(stmt, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 	}
 	
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#createStatement()
-	 */
 	public Statement createStatement() throws SQLException {
 		return getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 	}
 	
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#setAutoCommit(boolean)
-	 */
 	public void setAutoCommit(boolean value) throws SQLException {
-		if (!isTesting) getConnection().setAutoCommit(value);
+		getConnection().setAutoCommit(value);
 	}
 
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#commit()
-	 */
 	public void commit() throws SQLException {
-		if (!isTesting) getConnection().commit();
+		getConnection().commit();
 	}
 	
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#rollback()
-	 */
 	public void rollback() throws SQLException {
 		getConnection().rollback();
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fakeroot.sepm.server.IDBConnection#isTesting()
-	 */
-	public boolean isTesting() {
-		return isTesting;
-	}
-	
-	/**
-	 * returns true if DBConnection is set to testing mode.
-	 * 
-	 * Testing mode can be enabled/disabled in jdbc.properties.
-	 * If enabled, all queries are put into a transaction that is rolled back
-	 * when the connection is released (using disconnect()).
-	 * 
-	 * This is the static version of the function which means it may have to initialize the database connection pool.
-	 * That's why it's possible that this function throws Exceptions.
-	 * 
-	 * @return true: DBConnection is in testing mode, false otherwise
-	 * @throws FileNotFoundException if the property file could not be read
-	 * @throws IOException if any other file system related problem occurs
-	 */
-	public static boolean staticIsTesting() throws FileNotFoundException, IOException {
-		_init();
-		return isTesting;
 	}
 }
