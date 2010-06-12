8e5bb5c7e95538f9c2f23b3ce0fc1de3938a1ef2
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-06-12 14:19:28 +0000

    added the exception class RequestException and the serializable RequestError

---
+++ src/org/tagaprice/shared/RequestError.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2010 TagAPrice.org
+ * 
+ * Licensed under the Creative Commons License. You may not
+ * use this file except in compliance with the License. 
+ *
+ * http://creativecommons.org/licenses/by-nc/3.0/
+*/
+
+/**
+ * Project: tagaprice
+ * Filename: RequestError.java
+ * Date: 12.06.2010
+*/
+package org.tagaprice.shared;
+
+public class RequestError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	public RequestError(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	@Override
+	public String getSerializeName() {
+		// TODO Auto-generated method stub
+		return "requestError";
+	}
+
+}
