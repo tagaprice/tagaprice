ca633f4c1089c18eba1643f888e60d8816db34a1
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-06-15 12:53:36 +0000

    added shared class Locale

---
+++ src/org/tagaprice/shared/Locale.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2010 TagAPrice.org
+ * 
+ * Licensed under the Creative Commons License. You may not
+ * use this file except in compliance with the License. 
+ *
+ * http://creativecommons.org/licenses/by-nc/3.0/
+*/
+
+/**
+ * Project: tagaprice
+ * Filename: Locale.java
+ * Date: 15.06.2010
+*/
+package org.tagaprice.shared;
+
+public class Locale implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private Integer fallbackId;
+	private String title;
+	private String localTitle;
+
+	public Locale() {
+		this(-1, -1, null, null);
+	}
+	
+	public Locale(int id, Integer fallbackId, String title, String localTitle) {
+		this.id = id;
+		this.fallbackId = fallbackId;
+		this.title = title;
+		this.localTitle = localTitle;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public Integer getFallbackId() {
+		return fallbackId;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getLocalTitle() {
+		return localTitle;
+	}
+	
+	@Override
+	public String getSerializeName() {
+		return "locale";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		boolean rc = true;
+		if (o instanceof Locale) {
+			Locale l = (Locale) o;
+			if (l.getId() != getId()) rc = false;
+			
+			if (getFallbackId() != null) {
+				if (!getFallbackId().equals(l.getFallbackId())) rc = false;
+			}
+			else if (l.getFallbackId() != null) rc = false;
+			
+			if (getTitle() != null) {
+				if (!getTitle().equals(l.getTitle())) rc = false;
+			}
+			else if (l.getTitle() != null) rc = false;
+			
+			if (getLocalTitle() != null) {
+				if (!getLocalTitle().equals(l.getLocalTitle())) rc = false;
+			}
+			else if (l.getLocalTitle() != null) rc = false;
+		}
+		else rc = false;
+		
+		return rc;
+	}
+
+}
