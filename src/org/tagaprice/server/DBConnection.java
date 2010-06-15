73deabb09b1fe401b344d507294de0a6f2a58212
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-06-15 12:23:10 +0000

    made DBConnection more modular (it's now possible to override the way the jdbc.properties file is read)

--- src/org/tagaprice/server/DBConnection.java
+++ src/org/tagaprice/server/DBConnection.java
@@ -45,31 +45,41 @@ public class DBConnection {
 	 * @throws FileNotFoundException if no property file is found
 	 * @throws IOException if any other file access error occurs
 	 */
-	private static void _init() throws FileNotFoundException, IOException {
+	private void _init() throws FileNotFoundException, IOException {
 		if (dataSource == null)	{
-			Properties prop = new Properties();
 
 			PGConnectionPoolDataSource ds = new PGConnectionPoolDataSource();
-			
-			ClassLoader cl = Thread.currentThread().getContextClassLoader();
-			InputStream propStream = cl.getResourceAsStream("/jdbc.properties");
-			if (propStream == null) {
-				throw new FileNotFoundException("Error: Couldn't open property file 'jdbc.properties'");
-			}
-
-			prop.load(propStream);
-			propStream.close();
-
-			ds.setServerName(prop.getProperty("host", "localhost"));
-			ds.setPortNumber(Integer.parseInt(prop.getProperty("port", "5432")));
-			ds.setDatabaseName(prop.getProperty("db"));
-			ds.setUser(prop.getProperty("user"));
-			ds.setPassword(prop.getProperty("pwd"));
-			ds.setSsl(prop.getProperty("ssl", "true").equals("true"));
-			if (prop.containsKey("sslFactory")) {
-				ds.setSslfactory(prop.getProperty("sslFactory"));
-			}
+			_applyProperties(ds, "/jdbc.properties");
 			dataSource = ds;
+		}
+	}
+	
+	protected InputStream _loadResourceFile(String fileName) throws FileNotFoundException {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		InputStream is = cl.getResourceAsStream(fileName);
+		
+		if (is == null) {
+			throw new FileNotFoundException("Error: Couldn't open property file '"+fileName+"'");
+		}
+
+		return is;
+	}
+	
+	private void _applyProperties(PGConnectionPoolDataSource ds, String fileName) throws IOException {
+		Properties prop = new Properties();
+		InputStream propStream = _loadResourceFile(fileName);
+
+		prop.load(propStream);
+		propStream.close();
+
+		ds.setServerName(prop.getProperty("host", "localhost"));
+		ds.setPortNumber(Integer.parseInt(prop.getProperty("port", "5432")));
+		ds.setDatabaseName(prop.getProperty("db"));
+		ds.setUser(prop.getProperty("user"));
+		ds.setPassword(prop.getProperty("pwd"));
+		ds.setSsl(prop.getProperty("ssl", "true").equals("true"));
+		if (prop.containsKey("sslFactory")) {
+			ds.setSslfactory(prop.getProperty("sslFactory"));
 		}
 	}
 	
