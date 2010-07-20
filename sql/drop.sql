8cc3566144583adb701ce9f8b5ff114e6627e794
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-07-20 14:45:41 +0000

    added account, entity.creator and entityRevision.creator

--- sql/drop.sql
+++ sql/drop.sql
@@ -9,8 +9,12 @@ DROP TABLE IF EXISTS currency;
 DROP TABLE IF EXISTS productType;
 DROP TABLE IF EXISTS property;
 DROP TABLE IF EXISTS unit;
+
 ALTER TABLE entity DROP CONSTRAINT fkey_currentRev;
 DROP TABLE IF EXISTS entityRevision;
+
+ALTER TABLE entity DROP CONSTRAINT fkey_entity_creator;
+DROP TABLE IF EXISTS account;
 
 DROP TABLE IF EXISTS entity;
 
