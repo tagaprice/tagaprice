5e326aa26e821c178d32bc780f0b48cd7aeadc23
juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-07-21 14:21:09 +0000

    Alternative SearchWidget

---
+++ src/org/tagaprice/client/SearchWidget2.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010 TagAPrice.org
+ * 
+ * Licensed under the Creative Commons License. You may not
+ * use this file except in compliance with the License. 
+ *
+ * http://creativecommons.org/licenses/by-nc/3.0/
+*/
+
+/**
+ * Project: TagAPrice
+ * Filename: SearchWidget2.java
+ * Date: 20.07.2010
+*/
+package org.tagaprice.client;
+
+import org.tagaprice.client.SelectiveVerticalPanel.SelectionType;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class SearchWidget2 extends Composite {
+
+	public enum SerachType {ALL, PRODCUT, SHOP}
+	
+	public SearchWidget2(
+			String search, 
+			SerachType serachType,
+			boolean showNew, 
+			boolean popup,
+			SelectionType selectionType) {
+		// TODO Auto-generated constructor stub
+	}
+}
