847368ef9b28827ef84004cfeffe6d0ae58d4980
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-07-22 13:54:47 +0000

    made the foreign keys referencing account (in entity and entityrev) initially deferred

--- sql/tables/40_account.sql
+++ sql/tables/40_account.sql
@@ -10,8 +10,8 @@ CREATE TABLE account (
 ALTER TABLE entity ADD creator BIGINT NOT NULL;
 ALTER TABLE entityRevision ADD creator BIGINT NOT NULL;
 
-ALTER TABLE entity ADD CONSTRAINT fkey_entity_creator FOREIGN KEY (creator) REFERENCES account (uid) DEFERRABLE;
-ALTER TABLE entityRevision ADD CONSTRAINT fkey_entityRevision_creator FOREIGN KEY (creator) REFERENCES account (uid) DEFERRABLE;
+ALTER TABLE entity ADD CONSTRAINT fkey_entity_creator FOREIGN KEY (creator) REFERENCES account (uid) DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE entityRevision ADD CONSTRAINT fkey_entityRevision_creator FOREIGN KEY (creator) REFERENCES account (uid) DEFERRABLE INITIALLY DEFERRED;
 
 COMMIT;
 
