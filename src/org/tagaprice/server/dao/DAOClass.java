03090c763018874b596ee0774a87f8f98d55c290
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-07-22 13:56:29 +0000

    created interface DAOClass

---
+++ src/org/tagaprice/server/dao/DAOClass.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010 TagAPrice.org
+ * 
+ * Licensed under the Creative Commons License. You may not
+ * use this file except in compliance with the License. 
+ *
+ * http://creativecommons.org/licenses/by-nc/3.0/
+*/
+
+/**
+ * Project: tagaprice
+ * Filename: DAOClass.java
+ * Date: 21.07.2010
+*/
+package org.tagaprice.server.dao;
+
+import java.sql.SQLException;
+
+import org.tagaprice.server.DBConnection;
+import org.tagaprice.shared.Entity;
+import org.tagaprice.shared.exception.InvalidLocaleException;
+import org.tagaprice.shared.exception.NotFoundException;
+import org.tagaprice.shared.exception.RevisionCheckException;
+
+/**
+ * Specifies the methods all DAO-Classes have to implement
+ * @author Manuel Reithuber
+ */
+public interface DAOClass<T extends Entity> {
+	
+	public void get(T entity) throws SQLException, NotFoundException;
+	public void save(T entity) throws SQLException, NotFoundException, RevisionCheckException, InvalidLocaleException;
+}
