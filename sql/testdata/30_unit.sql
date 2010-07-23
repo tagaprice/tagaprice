cf4ab251a0114506958a13b15dcb5f3f9b1a47c3
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-07-23 13:16:51 +0000

    updated unit testdata (to support unitRevision)

--- sql/testdata/30_unit.sql
+++ sql/testdata/30_unit.sql
@@ -8,7 +8,8 @@ INSERT INTO entityRevision (ent_id, rev, title, creator) VALUES (
 	currval('entity_ent_id_seq'), 1, 'kg',
 	(SELECT ent_id FROM entityRevision WHERE title = 'tagaprice system')
 );
-INSERT INTO unit(unit_id) VALUES (currval('entity_ent_id_seq'));
+INSERT INTO unit(unit_id, currentRevision) VALUES (currval('entity_ent_id_seq'), 1);
+INSERT INTO unitRevision(unit_id, rev) VALUES (currval('entity_ent_id_seq'), 1);
 
 INSERT INTO entity (current_revision, locale_id, creator) VALUES (1,
 	(SELECT locale_id FROM locale WHERE title = 'English'),
@@ -18,7 +19,8 @@ INSERT INTO entityRevision (ent_id, rev, title, creator) VALUES (
 	currval('entity_ent_id_seq'), 1, 'g',
 	(SELECT ent_id FROM entityRevision WHERE title = 'tagaprice system')
 );
-INSERT INTO unit(unit_id, fallback_unit, factor) VALUES (currval('entity_ent_id_seq'), (SELECT ent_id FROM entityRevision WHERE title = 'kg' LIMIT 1), 0.001);
+INSERT INTO unit(unit_id, currentRevision) VALUES (currval('entity_ent_id_seq'), 1);
+INSERT INTO unitRevision(unit_id, rev, base_id, factor) VALUES (currval('entity_ent_id_seq'), 1, (SELECT ent_id FROM entityRevision WHERE title = 'kg' LIMIT 1), 0.001);
 
 INSERT INTO entity (current_revision, locale_id, creator) VALUES (1,
 	(SELECT locale_id FROM locale WHERE title = 'English'),
@@ -28,6 +30,7 @@ INSERT INTO entityRevision (ent_id, rev, title, creator) VALUES (
 	currval('entity_ent_id_seq'), 1, 't',
 	(SELECT ent_id FROM entityRevision WHERE title = 'tagaprice system')
 );
-INSERT INTO unit(unit_id, fallback_unit, factor) VALUES (currval('entity_ent_id_seq'), (SELECT ent_id FROM entityRevision WHERE title = 'kg' LIMIT 1), 1000);
+INSERT INTO unit(unit_id, currentRevision) VALUES (currval('entity_ent_id_seq'), 1);
+INSERT INTO unitRevision(unit_id, rev, base_id, factor) VALUES (currval('entity_ent_id_seq'), 1, (SELECT ent_id FROM entityRevision WHERE title = 'kg' LIMIT 1), 1000);
 
 COMMIT;
