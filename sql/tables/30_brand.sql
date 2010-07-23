a94eff3014550d18a37b52adb81b93ed67b6f1f0
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-07-23 11:19:36 +0000

    fixed a broken foreign key in 40_brand.sql and renamed it to 30_brand.sql

--- sql/tables/30_brand.sql
+++ sql/tables/30_brand.sql
@@ -2,5 +2,5 @@ CREATE TABLE brand (
 	brand_id BIGINT NOT NULL,
 	
 	PRIMARY KEY (brand_id),
-	FOREIGN KEY (brand_id) REFERENCES brand(brand_id)
+	FOREIGN KEY (brand_id) REFERENCES entity(ent_id)
 );
