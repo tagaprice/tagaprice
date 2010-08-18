f5abf8c043306c3a547dbe3ebcad40d0368f060f
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-18 19:12:45 +0000

    created table shopRevision (and moved shop's fields into it)

--- src/main/sql/tables/30_shop.sql
+++ src/main/sql/tables/30_shop.sql
@@ -1,15 +1,6 @@
 create table shop (
 	shop_id BIGINT NOT NULL,
-	type_id BIGINT, -- TODO create a shopType table and reference it from here
-	lat FLOAT,
-	lng FLOAT,
-	street varchar(100),
-	city varchar(100),
-	country_code char(2),
 
 	PRIMARY KEY (shop_id),
-	FOREIGN KEY (shop_id) REFERENCES entity (ent_id),
-	FOREIGN KEY (country_code) REFERENCES country(country_code),
-	CHECK (lat BETWEEN -90 AND 90),
-	CHECK (lng BETWEEN -180 AND 180)
+	FOREIGN KEY (shop_id) REFERENCES entity (ent_id)
 );
---
+++ src/main/sql/tables/40_shoprevision.sql
@@ -0,0 +1,18 @@
+create table shopRevision (
+	shop_id BIGINT NOT NULL,
+	rev integer NOT NULL,
+	type_id BIGINT, -- TODO create a shopType table and reference it from here
+	lat FLOAT,
+	lng FLOAT,
+	street varchar(100),
+	city varchar(100),
+	country_code char(2),
+
+	PRIMARY KEY (shop_id, rev),
+	FOREIGN KEY (shop_id) REFERENCES shop (shop_id),
+	FOREIGN KEY (shop_id, rev) REFERENCES entityRevision(ent_id, rev),
+	FOREIGN KEY (country_code) REFERENCES country(country_code),
+	CHECK (lat BETWEEN -90 AND 90),
+	CHECK (lng BETWEEN -180 AND 180)
+);
+
