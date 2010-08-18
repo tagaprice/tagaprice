a5b1048d4785f97e553679e09cf17750db111055
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-18 17:07:28 +0000

    product testdata uses entity's sequence now

--- src/main/sql/testdata/40_products.sql
+++ src/main/sql/testdata/40_products.sql
@@ -1,18 +1,26 @@
 BEGIN;
+<<<<<<< .mine
+INSERT INTO entity (locale_id, created_at, current_revision, creator) 
+=======
 /* INSERT PRODUCT */
 INSERT INTO entity (ent_id, locale_id, created_at, current_revision, creator) 
+>>>>>>> .r353
 VALUES 
-(9,1,'2010-08-18',1,1);
+(1,'2010-08-18',1,1);
 
 INSERT INTO entityrevision (ent_id, rev, title, created_at, creator) 
 VALUES
-(9,1, 'First Product', '2010-08-18', 1);
+(currval('entity_ent_id_seq'), 1, 'First Product', '2010-08-18', 1);
 
 INSERT INTO product (prod_id) VALUES (9);
 
 INSERT INTO productrevision (prod_id, rev, type_id, imageurl)
 VALUES
+<<<<<<< .mine
+(currval('entity_ent_id_seq'), 1,'nix.png');
+=======
 (9, 1, 20, 'nix.png');
+>>>>>>> .r353
 
 
 
@@ -29,43 +37,43 @@ INSERT INTO producttype (type_id) VALUES (20);
 
 /* INSERT PROPERTIES*/
 /* ***** Prop 1 ***** */
-INSERT INTO entity (ent_id, locale_id, created_at, current_revision, creator) 
+INSERT INTO entity (locale_id, created_at, current_revision, creator) 
 VALUES 
-(10,1,'2010-08-18',1,1);
+(1,'2010-08-18',1,1);
 
 INSERT INTO entityrevision (ent_id, rev, title, created_at, creator) 
 VALUES
-(10,1, 'energy', '2010-08-18', 1);
+(currval('entity_ent_id_seq'), 1, 'energy', '2010-08-18', 1);
 
-INSERT INTO property (prop_id) VALUES (10);
+INSERT INTO property (prop_id) VALUES (currval('entity_ent_id_seq'));
 
-INSERT INTO entityproperty (eprop_id, prop_id, ent_id, value, min_rev)
+INSERT INTO entityproperty (prop_id, ent_id, value, min_rev)
 VALUES
-(1, 10, 9, '20', 1);
+(currval('entity_ent_id_seq'), 9, '20', 1);
 
 INSERT INTO propertyrevision (prop_id, rev, name, minvalue, maxvalue, type, uniq)
 VALUES
-(10,1, 'energy',0,10, 'double',true);
+(currval('entity_ent_id_seq'),1, 'energy',0,10, 'double',true);
 
 
 /* ***** Prop 2 ***** */
-INSERT INTO entity (ent_id, locale_id, created_at, current_revision, creator) 
+INSERT INTO entity (locale_id, created_at, current_revision, creator) 
 VALUES 
-(11,1,'2010-08-18',1,1);
+(1,'2010-08-18',1,1);
 
 INSERT INTO entityrevision (ent_id, rev, title, created_at, creator) 
 VALUES
-(11,1, 'kph', '2010-08-18', 1);
+(currval('entity_ent_id_seq'),1, 'kph', '2010-08-18', 1);
 
-INSERT INTO property (prop_id) VALUES (11);
+INSERT INTO property (prop_id) VALUES (currval('entity_ent_id_seq'));
 
-INSERT INTO entityproperty (eprop_id, prop_id, ent_id, value, min_rev)
+INSERT INTO entityproperty (prop_id, ent_id, value, min_rev)
 VALUES
-(2, 11, 9, '180', 1);
+(currval('entity_ent_id_seq'), 9, '180', 1);
 
 INSERT INTO propertyrevision (prop_id, rev, name, minvalue, maxvalue, type, uniq)
 VALUES
-(11,1, 'kph',0,10, 'int', true);
+(currval('entity_ent_id_seq'),1, 'kph',0,10, 'int', true);
 
 /* ************** */
 COMMIT;
