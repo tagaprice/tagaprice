0f8afe7b4cb4cd7c8439a9652464317a1b363bf4
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-18 06:27:42 +0000

    updated insert.sh to better support command line parameters

--- src/main/sql/insert.sh
+++ src/main/sql/insert.sh
@@ -5,11 +5,12 @@ IFS='
 
 function injectScript() {
 	dir=$1
+	shift
 	for file in "$dir/"*.sql; do
 		export ON_ERROR_STOP=1
 		echo -e "\033[32mInjecting '$file'\033[0m"
 		
-		if ! psql -v ON_ERROR_STOP=1 tagaprice -f "$file"; then
+		if ! psql -v ON_ERROR_STOP=1 tagaprice -f "$file" $*; then
 			echo -e "\033[31mError\033[0m"
 			return 1
 		fi
@@ -19,8 +20,7 @@ function injectScript() {
 function inject() {
 	dir="$1"
 	shift
-	#getScript "$dir"|psql tagaprice -f - $*
-	injectScript "$dir"
+	injectScript "$dir" $*
 	return $?
 }
 
