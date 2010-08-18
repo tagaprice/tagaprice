319b37421ca4b6ad60af56e068e8163aca9d0513
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-18 18:53:02 +0000

    added CountryDAO and CountryDAOTest

---
+++ src/main/java/org/tagaprice/server/dao/CountryDAO.java
@@ -0,0 +1,32 @@
+package org.tagaprice.server.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.tagaprice.server.DBConnection;
+import org.tagaprice.shared.Country;
+import org.tagaprice.shared.exception.NotFoundException;
+
+public class CountryDAO {
+	private static CountryDAO instance;
+	private DBConnection db;
+
+	private CountryDAO(DBConnection db) {
+		this.db = db;
+	}
+	
+	public static CountryDAO getInstance(DBConnection db) {
+		if (instance == null) instance = new CountryDAO(db);
+		return instance;
+	}
+	
+	public Country get(String code) throws SQLException, NotFoundException {
+		PreparedStatement pstmt = db.prepareStatement("SELECT country_code, title, localTitle FROM country WHERE country_code = ?");
+		pstmt.setString(1, code);
+		ResultSet res = pstmt.executeQuery();
+		
+		if (!res.next()) throw new NotFoundException("Country not found: "+code);
+		return new Country(res.getString("country_code"), res.getString("title"), res.getString("localtitle"));
+	}
+}
---
+++ src/test/java/org/tagaprice/server/dao/CountryDAOTest.java
@@ -0,0 +1,35 @@
+package org.tagaprice.server.dao;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.tagaprice.server.dao.EntityDAOTest.TestDBConnection;
+import org.tagaprice.shared.Country;
+import org.tagaprice.shared.exception.NotFoundException;
+
+public class CountryDAOTest {
+	TestDBConnection db;
+	CountryDAO dao;
+	@Before
+	public void setUp() throws Exception {
+		db = new TestDBConnection();
+		dao = CountryDAO.getInstance(db);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGet() throws Exception {
+		Country c = dao.get("us");
+		assertEquals("us", c.getCode());
+		assertEquals("United States", c.getTitle());
+		assertEquals(null, c.getLocalTitle());
+	}
+
+}
