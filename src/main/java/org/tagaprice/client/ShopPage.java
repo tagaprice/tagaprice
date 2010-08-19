3e5e2f7ffd3d7770ef26de911b1c6da1de8f6559
juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-19 14:28:36 +0000

    Implemented RPC call in ShopPage Add MorphWidget for the title tag in ShopPage

--- src/main/java/org/tagaprice/client/ShopPage.java
+++ src/main/java/org/tagaprice/client/ShopPage.java
@@ -25,17 +25,21 @@ import org.tagaprice.client.propertyhandler.ListPropertyHandler;
 import org.tagaprice.client.propertyhandler.PropertyChangeHandler;
 import org.tagaprice.shared.PropertyData;
 import org.tagaprice.shared.PropertyGroup;
+import org.tagaprice.shared.PropertyValidator;
 import org.tagaprice.shared.SearchResult;
 import org.tagaprice.shared.ShopData;
 import org.tagaprice.shared.Type;
+import org.tagaprice.shared.PropertyDefinition.Datatype;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -52,6 +56,7 @@ public class ShopPage extends InfoBoxComposite {
 	private SimplePanel propertyHandlerContainer = new SimplePanel();
 	private TaPManager TaPMng = TaPManagerImpl.getInstance();
 	private PriceMapWidget priceMap;
+	private MorphWidget titleMorph = new MorphWidget("", Datatype.STRING, true);
 	
 	public ShopPage(ShopData _shopData, Type _type){
 		init(vePa1);
@@ -83,17 +88,66 @@ public class ShopPage extends InfoBoxComposite {
 			}
 		};
 		
-		//style
+		//style		
+		vePa1.setWidth("100%");
+		
+		//Header
+		HorizontalPanel hoPa1 = new HorizontalPanel();
+		hoPa1.setWidth("100%");
+		vePa1.add(titleMorph);		
+		titleMorph.setText(shopData.getTitle());
+		titleMorph.setWidth("100%");
+		vePa1.add(hoPa1);
+		ProgressWidget progressWidget = new ProgressWidget(new Image(MyResources.INSTANCE.productPriview()), shopData.getProgress());
+		hoPa1.add(progressWidget);
+		
+		VerticalPanel vePa2 = new VerticalPanel();
+		vePa2.setWidth("100%");
+		hoPa1.add(vePa2);
+		hoPa1.setCellWidth(vePa2, "100%");
+		
 		//Type
-		vePa1.add(typeWidgetContainer);	
+		vePa2.add(typeWidgetContainer);	
 		drawTypeWidget();
 		
-		vePa1.setWidth("100%");
-		vePa1.add(new ShopPreview(this.shopData, true));
+		//Rating
+		vePa2.add(new RatingWidget(shopData.getRating(), false));
+		
+		
+		//vePa1.add(new ShopPreview(this.shopData, true));
+		
+		
+		//Listener
+		titleMorph.addMorphWidgetErrorHandler(new MorphWidgetErrorHandler() {
+			
+			@Override
+			public void onSuccess(Datatype errorType) {
+				
+				if(!shopData.getTitle().equals(titleMorph.getText())){
+					shopData.setTitle(titleMorph.getText());
+					showSave();				
+				}
+			}
+			
+			@Override
+			public void onError(Datatype errorType) {
+				// TODO Auto-generated method stub
+			}
+			
+			@Override
+			public void onEmpty() {
+				// TODO Auto-generated method stub
+			}
+		});
+		
+		
+		
 		
 		//Add Price
-		priceMap = new PriceMapWidget(shopData.getId(),PriceMapType.SHOP);
-		vePa1.add(priceMap);
+		if(shopData.getId()!=null){
+			priceMap = new PriceMapWidget(shopData.getId(),PriceMapType.SHOP);
+			vePa1.add(priceMap);
+		}
 		
 		
 		//Create and Register Handler
@@ -199,6 +253,33 @@ public class ShopPage extends InfoBoxComposite {
 			@Override
 			public void onClick(ClickEvent event) {
 				topSave.setText("Saving...");
+				
+				shopData.setProperties(hashToPropertyList(hashProperties));
+				
+				//Validate Data
+				if(PropertyValidator.isValid(type, shopData.getProperties())){	
+					TaPMng.saveShop(shopData, new AsyncCallback<ShopData>() {
+						
+						@Override
+						public void onSuccess(ShopData result) {
+							bottomInfo.setVisible(false);
+							topSave.setText("Save");
+							if(shopData.getId()==null){
+								History.newItem("shop/get&id="+result.getId());
+							}
+						}
+						
+						@Override
+						public void onFailure(Throwable caught) {
+							showInfo("Save Fail: "+caught, BoxType.WARNINGBOX);							
+						}
+					});
+					
+					
+				}else{
+					showInfo("Save Fail: Invalide Data", BoxType.WARNINGBOX);
+				}
+				
 				
 				//TODO test
 				
