519efa47f103f782d6fbde42d8e5a503cf37af9a
manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <manuel_reithuber@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-23 19:59:11 +0000

    added a password hash and salt check to the JUnit test

--- src/test/java/org/tagaprice/server/dao/LocalAccountDAOTest.java
+++ src/test/java/org/tagaprice/server/dao/LocalAccountDAOTest.java
@@ -2,6 +2,12 @@ package org.tagaprice.server.dao;
 
 
 import static org.junit.Assert.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +19,7 @@ public class LocalAccountDAOTest {
 	private DBConnection db;
 	private LocalAccountDAO dao;
 	private int localeId;
+	private final String pwd = "my secret password";
 
 	@Before
 	public void setUp() throws Exception {
@@ -27,11 +34,38 @@ public class LocalAccountDAOTest {
 	
 	@Test
 	public void testCreate() throws Exception {
-		LocalAccountData account = new LocalAccountData("testAccount", localeId, null, "mail@foo.invalid", "my secret password", null);
+		LocalAccountData account = new LocalAccountData("testAccount", localeId, null, "mail@foo.invalid", pwd, null);
 		dao.save(account);
 		LocalAccountData a2 = new LocalAccountData(account.getId());
 		dao.get(a2);
 		assertEquals("Accounts don't match!", account, a2);
+		
+		// check if the password is saved correctly
+		PreparedStatement pstmt = db.prepareStatement("SELECT password, salt FROM localAccount WHERE uid = ?");
+		pstmt.setLong(1, account.getId());
+		ResultSet res = pstmt.executeQuery();
+		assertTrue("Error: localAccount wasn't saved!", res.next());
+		
+		String salt = res.getString("salt");
+		String pwdHash = res.getString("password");
+
+		System.out.println("Pwd: "+pwd+"\nSalt: "+salt+"\nHash: "+pwdHash);
+        assertEquals(md5(pwd+salt), pwdHash);
+
 	}
 
+	public static String md5(String in) throws NoSuchAlgorithmException {
+		// calculate hash 
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+	    md5.update(in.getBytes());
+	    byte[] hash = md5.digest();
+	    StringBuffer rc = new StringBuffer();
+        for (int i=0; i<hash.length; i++) {
+        	String hex = "0"+Integer.toHexString(0xFF & hash[i]);
+        	if (hex.length()>2) hex = hex.substring(hex.length()-2);
+            rc.append(hex);
+        }
+        
+        return rc.toString();
+	}
 }
