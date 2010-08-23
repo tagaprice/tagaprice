680bb6089a331974e144254a7afaaf73127cce04
juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-23 13:06:33 +0000

    Changed name from account to accountData

---
+++ src/main/java/org/tagaprice/shared/AccountData.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2010 TagAPrice.org
+ * 
+ * Licensed under the Creative Commons License. You may not
+ * use this file except in compliance with the License. 
+ *
+ * http://creativecommons.org/licenses/by-nc/3.0/
+*/
+
+/**
+ * Project: tagaprice
+ * Filename: Account.java
+ * Date: 21.07.2010
+*/
+package org.tagaprice.shared;
+
+public class AccountData extends Entity {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor needed for serialization
+	 */
+	public AccountData() {}
+
+	/**
+	 * constcructor used to query an account's current revision from the database
+	 * (using AccountDAO) 
+	 * @param id Account ID
+	 */
+	public AccountData(Long id) {
+		super(id);
+	}
+	
+	/**
+	 * constructor used to query a specific revision from the DB
+	 *(using AccountDAO)
+	 * @param id Account ID
+	 * @param rev account revision
+	 */
+	public AccountData(Long id, int rev) {
+		super(id, rev);
+	}
+
+	/**
+	 * constructor used to create a new Account
+	 * @param title Account's descriptive name (e.g. "Administrator")
+	 * @param localeId the Locale of an account just tells us which locale the user prefers
+	 *     (a user isn't locked into a single local version of the site)
+	 */
+	public AccountData(String title, int localeId) {
+		super(title, localeId, null);
+	}
+
+	/**
+	 * constructor used to update an existing Account
+	 * @param id Account ID
+	 * @param rev current Account revision (will be checked by AccountDAO to detect concurrent storage requests)
+	 * @param title Account's (new) descriptive name (e.g. "Administrator")
+	 * @param revCreatorId revision creator (most times this will be the user himself, but sometimes admins will change a user's details)
+	 */
+	public AccountData(Long id, int rev, String title, Long revCreatorId) {
+		super(id, rev, title, revCreatorId);
+	}
+
+	@Override
+	public String getSerializeName() {
+		return "account";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		boolean rc = false;
+		if (o instanceof AccountData) rc = super.equals(o);
+		return rc;
+	}
+
+	@Override
+	public String toString() {
+		return "Account {\n" +
+				super.toString()+
+				"}\n";
+	}
+}
