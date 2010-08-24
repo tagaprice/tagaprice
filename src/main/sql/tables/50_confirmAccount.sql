e9747cbf684c9b8de71bbe9d9b7f0cb7caf8cf1a
juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4 <juergen_brandstetter@7cb9cfb0-170a-48f0-82cf-47a104da51d4>
2010-08-24 18:22:36 +0000

    add auto timestamp to confirmAccount.sql

--- src/main/sql/tables/50_confirmAccount.sql
+++ src/main/sql/tables/50_confirmAccount.sql
@@ -2,7 +2,7 @@ CREATE TABLE confirmaccount
 (
  uid bigint NOT NULL,
  confirm character varying(200),
- confirm_date timestamp without time zone,
+ confirm_date timestamp without time zone NOT NULL DEFAULT now(),
  CONSTRAINT confirmaccount_pkey PRIMARY KEY (uid),
  CONSTRAINT confirmaccount_uid_fkey FOREIGN KEY (uid)
       REFERENCES account (uid) MATCH SIMPLE
