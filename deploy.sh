3dd8c83f22178f3f5ce2a47b6a80ac448ea6deb8
tagaprice@vienna.durchlaufnetz <tagaprice@vienna.durchlaufnetz>
2010-10-05 19:20:32 +0200

    - deploy.sh checks for an exexutable .post-compile.sh and calls it inststead of simply copying the .war to the default tomcat webapp directory - added .post-compile.sh to the ignore list

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+USERSCRIPT=./.post_compile.sh
+
 mvn -D maven.test.skip=true package
-cp target/tagaprice-0.0.1-SNAPSHOT.war /var/lib/tomcat6/webapps/tagaprice.war
+
+if [ -x "$USERSCRIPT" ]; then
+	$USERSCRIPT
+else
+	cp target/tagaprice-0.0.1-SNAPSHOT.war /var/lib/tomcat6/webapps/tagaprice.war
+fi
