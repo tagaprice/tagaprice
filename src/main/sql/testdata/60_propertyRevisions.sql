21779102d746348c46b33f1a456932f331cbb492
BrandiATMuhkuh <juergen@tagaprice.org>
2010-11-07 20:59:00 +0100

    add testdate for propertyRevisions

---
+++ src/main/sql/testdata/60_propertyRevisions.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+INSERT INTO entity 
+(locale_id,current_revision,creator)
+VALUES 
+(1,1,1);
+
+INSERT INTO entityrevision 
+("ent_id","rev","title","creator") 
+VALUES 
+(currval('entity_ent_id_seq'),1,'proteins',1);
+
+INSERT INTO propertyrevision 
+("prop_id","rev","name","minvalue","maxvalue","type","uniq") 
+VALUES 
+(currval('entity_ent_id_seq'),1,'Proteins',0,1000,'double',TRUE);
+
+COMMIT;
