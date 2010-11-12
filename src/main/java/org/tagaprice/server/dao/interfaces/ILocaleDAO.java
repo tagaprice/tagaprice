788820f5051b89b7b77b797dcd570dacedb7a4dc
Martin Weik <martin@weik.at>
2010-11-12 14:00:18 +0100

    Added standard ISO-Code getters for LocaleDAO

--- src/main/java/org/tagaprice/server/dao/interfaces/ILocaleDAO.java
+++ src/main/java/org/tagaprice/server/dao/interfaces/ILocaleDAO.java
@@ -4,27 +4,49 @@ import org.tagaprice.shared.Locale;
 import org.tagaprice.shared.exception.DAOException;
 
 /**
- * @author Martin Weik (afraidoferrors)
- * DAO vor getting and setting locales
+ * @author Martin Weik (afraidoferrors) DAO vor getting and setting locales
  */
 public interface ILocaleDAO {
 	/**
 	 * Gets a Locale by its id identified by an integer
-	 * @param id an integer
+	 * 
+	 * @param id
+	 *            an integer
 	 * @return returns a Locale for this id or null if this id does not exist
 	 * @throws DAOException
 	 */
 	public Locale getById(int id) throws DAOException;
+
 	/**
-	 * Gets a Locale by its name
-	 * @param name identifiying String for the Locale
-	 * @return returns a Locale for this name or null if this name does not exist
+	 * Gets a Locale by its full language name in its own language (e.g. "English", "Deutsch").
+	 * 
+	 * @param name
+	 *            identifiying String for the Locale
+	 * @return returns a Locale for this name or null if this name does not
+	 *         exist
 	 * @throws DAOException
 	 */
-	public Locale getByName(String name) throws DAOException;
+	public Locale getByFullLanguageName(String name) throws DAOException;
 	/**
-	 * Stores a Locale to the database
-	 * TODO be more precise
+	 * Gets a Locale by it's Countrycode and it's Languagecode, see {@link http://download.oracle.com/javase/6/docs/api/java/util/Locale.html} for valid codes.
+	 * @param language the languagecode, see {@link http://www.loc.gov/standards/iso639-2/englangn.html}
+	 * @param country the countrycode, see {@link http://www.iso.ch/iso/en/prods-services/iso3166ma/02iso-3166-code-lists/list-en1.html}
+	 * @return
+	 * @throws DAOException
+	 */
+	public Locale getByCountryAndLanguageCode(String country, String language) throws DAOException;
+
+	/**
+	 * Gets a Locale by its Languagecode, see {@link http://download.oracle.com/javase/6/docs/api/java/util/Locale.html} for valid codes.
+	 * @param language the languagecode, see {@link http://www.loc.gov/standards/iso639-2/englangn.html}
+	 * @return
+	 * @throws DAOException
+	 */
+	public Locale getByLanguageCode(String language) throws DAOException;
+
+	/**
+	 * Stores a Locale to the database TODO be more precise
+	 * 
 	 * @param locale
 	 * @throws DAOException
 	 */
