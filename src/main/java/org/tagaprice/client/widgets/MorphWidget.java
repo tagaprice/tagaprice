de58679f145660376d1a9931069796fda2e6288c
Jürgen Brandstetter <juergen@tagaprice.org>
2010-11-12 02:54:30 +0100

    Removed unused comments MorphWidget

--- src/main/java/org/tagaprice/client/widgets/MorphWidget.java
+++ src/main/java/org/tagaprice/client/widgets/MorphWidget.java
@@ -1,17 +1,3 @@
-/*
- * Copyright 2010 TagAPrice.org
- * 
- * Licensed under the Creative Commons License. You may not
- * use this file except in compliance with the License. 
- *
- * http://creativecommons.org/licenses/by-nc/3.0/
- */
-
-/**
- * Project: TagAPriceUI
- * Filename: MorphWidget.java
- * Date: 14.05.2010
- */
 package org.tagaprice.client.widgets;
 
 import org.tagaprice.shared.PropertyDefinition.Datatype;
