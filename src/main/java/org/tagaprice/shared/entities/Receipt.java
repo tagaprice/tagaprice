4622d7d9132e5a4ae77d0c2a5c1d29a116a16ab7
haja <h.jagenteufel@gmail.com>
2010-11-19 16:53:00 +0100

    docu + renamings in Receipt; added TODO for bill and isDraft

--- src/main/java/org/tagaprice/shared/entities/Receipt.java
+++ src/main/java/org/tagaprice/shared/entities/Receipt.java
@@ -2,187 +2,203 @@
  * Copyright 2010 TagAPrice.org
  * 
  * Licensed under the Creative Commons License. You may not
- * use this file except in compliance with the License. 
+ * use this file except in compliance with the License.
  *
  * http://creativecommons.org/licenses/by-nc/3.0/
-*/
+ */
 
 /**
  * Project: TagAPriceUI
  * Filename: ReceiptContainer.java
  * Date: 16.05.2010
-*/
+ */
 package org.tagaprice.shared.entities;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 /**
+ * A {@link Receipt} represents a list of products with an associated date (the date, when the products where bought).
+ * It also holds information about the {@link Shop}, where the {@link Product}s where bought.
  * 
- * Contains all important information to represent a receipt. 
- *
+ * A {@link Receipt} can also be a draft. TODO document this case better.
  */
 public class Receipt extends Entity {
 	private static final long serialVersionUID = 1L;
 
-	private boolean draft;
-	private Date date;
-	private int bill; //in Cent
-	private Shop shop;
-	private ArrayList<Product> productData = new ArrayList<Product>();
-	
+	private boolean _isDraft;
+	private Date _date;
+	/** in Cent (or similar) TODO what's that for? */
+	private int _bill;
+	private Shop _shop;
+	private ArrayList<Product> _products = new ArrayList<Product>();
+
 	/**
-	 * default constructor (needed for serialization)
+	 * default constructor needed for serialization
 	 */
 	public Receipt() {
 		super();
 	}
-	
+
 	/**
-	 * constructor used for querying the current revision of a Receipt from the database (using ReceiptDAO)
+	 * constructor used for querying the current revision of a {@link Receipt}from the database
 	 * @param id Receipt ID
 	 */
 	public Receipt(long id) {
 		super(id);
 	}
-	
+
 	/**
-	 * constructor used for querying a specific revision of a Receipt from the database (using ReceiptDAO)
+	 * constructor used for querying a specific revision of a {@link Receipt} from the database
 	 * @param id Receipt ID
 	 * @param rev requested revision
 	 */
 	public Receipt(long id, int rev) {
 		super(id, rev);
 	}
-	
+
 	/**
-	 * constructor used to store a new Receipt into the database (using ReceiptDAO) 
+	 * constructor used to store a new {@link Receipt} into the database
 	 * @param title descriptive receipt name (should not be empty)
 	 * @param localeId receipt locale
 	 * @param creatorId receipt's creator
 	 * @param date receipt date
-	 * @param bill TODO what's that for?
-	 * @param shop Shop ID
-	 * @param products
-	 * @param draft
+	 * @param bill bill in Cent (or similar)
+	 * @param shop {@link Shop} this {@link Receipt} is from
+	 * @param products list of {@link Product}s of this bill.
+	 * @param draft is this {@link Receipt} a draft only?
 	 */
 	public Receipt(String title, int localeId, long creatorId, Date date, int bill, Shop shop, ArrayList<Product> products, boolean draft) {
 		super(title, localeId, creatorId);
-		this.date = date;
-		this.bill = bill;
-		this.shop = shop;
-		this.productData = products;
+		_date = date;
+		_bill = bill;
+		_shop = shop;
+		_products = products;
+		_isDraft = draft;
 	}
-	
+
+	/**
+	 * constructor used to create a {@link Receipt} with given ID and revision
+	 * @param id ID
+	 * @param rev revision
+	 * @param title descriptive receipt name (should not be empty)
+	 * @param localeId receipt locale
+	 * @param creatorId receipt's creator
+	 * @param date receipt date
+	 * @param bill bill in Cent (or similar)
+	 * @param shop {@link Shop} this {@link Receipt} is from
+	 * @param products list of {@link Product}s of this bill.
+	 * @param draft is this {@link Receipt} a draft only?
+	 */
 	public Receipt(long id, int rev, String title, long creatorId, Date date, int bill, Shop shop, ArrayList<Product> products, boolean draft) {
 		super(id, rev, title, creatorId);
-		this.date = date;
-		this.bill = bill;
-		this.shop = shop;
-		this.productData = products;
+		_date = date;
+		_bill = bill;
+		_shop = shop;
+		_products = products;
+		_isDraft = draft;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * @return true, if this {@link Receipt} is a draft.
 	 */
 	public boolean getDraft() {
-		return draft;
+		return _isDraft;
 	}
-	
+
 	/**
-	 * 
-	 * @param draft
+	 * @param draft set if this {@link Receipt} is a draft.
 	 */
 	public void setDraft(boolean draft) {
-		this.draft = draft;
+		_isDraft = draft;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * @return date of this {@link Receipt}.
 	 */
 	public Date getDate() {
-		return date;
+		return _date;
 	}
-	
+
 	/**
-	 * 
-	 * @param date
+	 * @param date date of this {@link Receipt}.
 	 */
 	public void setDate(Date date) {
-		this.date = date;
+		_date = date;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * Calculates the total price for all {@link Product}s of this {@link Receipt}
+	 * using the avgPrice of the {@link Product}s.
+	 * @return the total price for all {@link Product}s of this {@link Receipt}
+	 */
+	public long getTotalPrice(){
+
+		long totalPrice = 0;
+		for(Product pd: _products){
+			totalPrice= totalPrice+pd.getAvgPrice().getAmount();
+		}
+
+		return totalPrice;
+	}
+
+	/**
+	 * TODO use {@link Receipt#getTotalPrice()} instead ?
+	 * @return the amount of money for this {@link Receipt} in Cent (or similar)
 	 */
 	public int getBill() {
-		return bill;
+		return _bill;
 	}
-	
+
 	/**
-	 * 
-	 * @param bill
+	 * @param bill the amount of money for this {@link Receipt} in Cent (or similar)
 	 */
 	public void setBill(int bill) {
-		this.bill = bill;
+		_bill = bill;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * @return {@link Shop} where this receipt is from.
 	 */
 	public Shop getShop() {
-		return shop;
+		return _shop;
 	}
-	
+
 	/**
-	 * 
-	 * @param shop Shop
+	 * @param shop {@link Shop} where this receipt is from.
 	 */
 	public void setShop(Shop shop) {
-		this.shop = shop;
+		_shop = shop;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * @return all {@link Product}s of this {@link Receipt}.
 	 */
-	public ArrayList<Product> getProductData() {
-		return productData;
+	public ArrayList<Product> getProducts() {
+		return _products;
 	}
-	
+
 	/**
-	 * 
-	 * @param productPreviewData
+	 * @param products {@link Product}s of this {@link Receipt}.
 	 */
-	public void setProductData(
-			ArrayList<Product> productData) {
-		this.productData = productData;
+	public void setProductData(ArrayList<Product> products) {
+		_products = products;
 	}
+
 
 	@Override
 	public String getSerializeName() {
 		return "receipt";
 	}
-	
-	
-	public long getTotalPrice(){
-		
-		long totalPrice = 0;
-		for(Product pd: productData){
-			totalPrice= totalPrice+pd.getAvgPrice().getAmount();
-		}
-		
-		return totalPrice;
-	}
-	// TODO implement missing ReceiptData.equals()
 
+	/**
+	 * TODO implement this method
+	 */
 	@Override
 	public <T extends Entity> T newRevision() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	// TODO implement missing ReceiptData.equals()
 }
