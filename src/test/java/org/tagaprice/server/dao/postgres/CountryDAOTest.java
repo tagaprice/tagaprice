947ed224fc1ed0492c5cb0a15357d6b4366abee6
Martin Weik <martin@weik.at>
2010-11-21 12:32:03 +0100

    CountryDAO with 2 Tests.

--- src/test/java/org/tagaprice/server/dao/postgres/CountryDAOTest.java
+++ src/test/java/org/tagaprice/server/dao/postgres/CountryDAOTest.java
@@ -1,12 +1,15 @@
 package org.tagaprice.server.dao.postgres;
 
-import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.*;
 import org.junit.*;
 import org.tagaprice.server.dao.helper.DatabaseManager;
 import org.tagaprice.shared.entities.Country;
+import org.tagaprice.shared.exception.DAOException;
 
 public class CountryDAOTest {
 	IDataSet dataset;
+	CountryDAO countryDAO;
+	ITable countriesTable;
 
 	@Before
 	public void setUp() throws Exception {
@@ -15,6 +18,8 @@ public class CountryDAOTest {
 		 * For later use, we store the inserted dataset
 		 */
 		dataset = DatabaseManager.setupEntitytable(Country.class);
+		countriesTable = dataset.getTable("country");
+		countryDAO = new CountryDAO(DatabaseManager.getDBConnection());
 	}
 
 	@After
@@ -22,8 +27,31 @@ public class CountryDAOTest {
 	}
 
 	@Test
-	public void testGetByCountryCode() {
+	public void testGetByCountryCodeStandardCase() throws DataSetException {
+		String countryCode = countriesTable.getValue(0, "country_code").toString();
+		String title = countriesTable.getValue(0, "title").toString();
+		String localtitle = countriesTable.getValue(0, "localtitle").toString();
+		Country country = null;
+		try {
+			country = countryDAO.getByCountryCode(countryCode);
+		} catch (DAOException e) {
+			Assert.fail("CountryDAO threw an unexpected Exception");
+		}
+		Assert.assertNotNull("Existing country was not found", country);
+		Assert.assertEquals(title, country.getTitle());
+		Assert.assertEquals(localtitle, country.getLocalTitle());
+	}
 
+	@Test
+	public void testGetByCountryCodeErrorCase() {
+		String countryCode = "not_Exists";
+		Country country = null;
+		try {
+			country = countryDAO.getByCountryCode(countryCode);
+		} catch (DAOException e) {
+			Assert.fail("CountryDAO threw an unexpected Exception");
+		}
+		Assert.assertNull(country);
 	}
 
 }
