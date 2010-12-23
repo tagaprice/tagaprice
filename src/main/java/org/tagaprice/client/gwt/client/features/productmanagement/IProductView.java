b61ea5836216ef01a0ff083e13f02ed483bad1d7
Jürgen Brandstetter <juergen@tagaprice.org>
2010-12-23 11:54:02 +0100

    Add IProductView for ProductView implementation

--- src/main/java/org/tagaprice/client/gwt/client/features/productmanagement/IProductView.java
+++ src/main/java/org/tagaprice/client/gwt/client/features/productmanagement/IProductView.java
@@ -1,5 +1,8 @@
 package org.tagaprice.client.gwt.client.features.productmanagement;
 
+import org.tagaprice.client.gwt.shared.entities.dump.ICategory;
+import org.tagaprice.client.gwt.shared.entities.dump.IUnit;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.ui.IsWidget;
@@ -10,27 +13,79 @@ import com.google.gwt.user.client.ui.IsWidget;
  */
 public interface IProductView extends IsWidget {
 
+	/**
+	 * 
+	 * @param title
+	 */
 	public void setTitle(String title);
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getTitle();
 
-	//TODO
-	public void setUnit();
+	/**
+	 * 
+	 * @param iUnit
+	 */
+	public void setUnit(IUnit iUnit);
 
-	public void getUnit();
+	/**
+	 * 
+	 * @return
+	 */
+	public IUnit getUnit();
 
+	/**
+	 * 
+	 * @param category
+	 */
+	public void setCategory(ICategory category);
 
-	public void setCategory();
+	/**
+	 * 
+	 * @return
+	 */
+	public ICategory getCategory();
 
-	public void getCategory();
+	/**
+	 * 
+	 * @param presenter
+	 */
+	public void setPresenter(Presenter presenter);
 
-
+	/**
+	 * 
+	 *
+	 */
 	public interface Presenter {
+		/**
+		 * 
+		 * @param place
+		 */
 		public void goTo(Place place);
 
+		/**
+		 * 
+		 * @param event
+		 */
 		public void onSaveButtonClickedEvent(ClickEvent event);
 
+		/**
+		 * TODO
+		 */
 		public void onTitleSelectedEvent();
+
+		/**
+		 * TODO
+		 */
+		public void onUnitSelectedEvent();
+
+		/**
+		 * TODO
+		 */
+		public void onCategorySelectedEvent();
 	}
 
 }
