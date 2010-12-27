d19c53b522204557f1c879fb33cba3d9dc13bede
Martin Weik <martin@weik.at>
2010-12-27 15:21:11 +0100

    Changed Unit

--- src/main/java/org/tagaprice/client/gwt/shared/entities/dump/Unit.java
+++ src/main/java/org/tagaprice/client/gwt/shared/entities/dump/Unit.java
@@ -1,24 +1,29 @@
 package org.tagaprice.client.gwt.shared.entities.dump;
+/**
+ * Represents Units and converts between them.
+ * @author Martin
+ *
+ */
+public enum Unit  {
 
-public enum Unit implements IUnit {
 	piece {
 		@Override
 		public UnitType getType() {
-			return IUnit.UnitType.Piece;
+			return UnitType.Piece;
 		}
 
 		@Override
-		public double convert(Unit targetUnit) throws UnitNotConvertibleException {
+		public double getFactor(Unit targetUnit) throws UnitNotConvertibleException {
 			throw new UnitNotConvertibleException("A Piece is not convertible");
 		}
 	}, l{
 		@Override
 		public UnitType getType() {
-			return IUnit.UnitType.Volume;
+			return UnitType.Volume;
 		}
 
 		@Override
-		public double convert(Unit targetUnit) throws UnitNotConvertibleException {
+		public double getFactor(Unit targetUnit) throws UnitNotConvertibleException {
 			switch(targetUnit) {
 			case l:
 				return 1;
@@ -30,43 +35,90 @@ public enum Unit implements IUnit {
 	}, ml{
 		@Override
 		public UnitType getType() {
-			return IUnit.UnitType.Volume;
+			return UnitType.Volume;
 		}
 
 		@Override
-		public double convert(Unit targetUnit) throws UnitNotConvertibleException {
+		public double getFactor(Unit targetUnit) throws UnitNotConvertibleException {
+			switch (targetUnit) {
+			case ml:
+				return 1;
+			case l:
+				return 1000;
+			}
 			throw new UnitNotConvertibleException("It is not possible to convert " + this.getType().toString() + " into " + targetUnit.getType().toString());
 		}
 	}, g{
 		@Override
 		public UnitType getType() {
-			return IUnit.UnitType.Mass;
+			return UnitType.Mass;
 		}
 
 		@Override
-		public double convert(Unit targetUnit) throws UnitNotConvertibleException {
+		public double getFactor(Unit targetUnit) throws UnitNotConvertibleException {
+			switch (targetUnit) {
+			case g:
+				return 1;
+			case kg:
+				return 1000;
+			}
 			throw new UnitNotConvertibleException("It is not possible to convert " + this.getType().toString() + " into " + targetUnit.getType().toString());
 		}
 	}, kg{
 		@Override
 		public UnitType getType() {
-			return IUnit.UnitType.Mass;
+			return UnitType.Mass;
 		}
 
 		@Override
-		public double convert(Unit targetUnit) throws UnitNotConvertibleException {
+		public double getFactor(Unit targetUnit) throws UnitNotConvertibleException {
+			switch(targetUnit) {
+			case kg:
+				return 1;
+			case g:
+				return 0.001;
+			}
 			throw new UnitNotConvertibleException("It is not possible to convert " + this.getType().toString() + " into " + targetUnit.getType().toString());
 		}
 	};
 
-	@Override
+	/**
+	 * Gives the Type of this Unit
+	 * @return the Type of this Unit
+	 */
 	public UnitType getType() {
 		return null;
 	}
 
-	@Override
-	public double convert(Unit targetUnit) throws UnitNotConvertibleException {
+	/**
+	 * Converts a Unit to anoter Unit
+	 * @param targetUnit
+	 * @return the convertion factor
+	 * @throws UnitNotConvertibleException if a conversion is not possible
+	 */
+	public double getFactor(Unit targetUnit) throws UnitNotConvertibleException {
 		throw new UnitNotConvertibleException("Call on Unit directly is not allowed");
+	}
+
+	/**
+	 * Every Type is convertible with itself.
+	 * @author Martin
+	 *
+	 */
+	public enum UnitType {
+		Length, Area, Volume, Mass, Piece
+	}
+
+	public class UnitNotConvertibleException extends Exception {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1722614103910484555L;
+
+		public UnitNotConvertibleException(String message) {
+			super(message);
+		}
 	}
 
 
