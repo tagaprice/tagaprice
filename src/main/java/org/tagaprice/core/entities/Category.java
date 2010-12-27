c0bda489c678806473bf3a5ad76aab224b6264e4
haja <h.jagenteufel@gmail.com>
2010-12-27 18:17:39 +0100

    added category hibernate mapping

--- src/main/java/org/tagaprice/core/entities/Category.java
+++ src/main/java/org/tagaprice/core/entities/Category.java
@@ -1,5 +1,29 @@
 package org.tagaprice.core.entities;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="typerevision")
+@PrimaryKeyJoinColumns({
+	@PrimaryKeyJoinColumn(name="type_id", referencedColumnName="ent_id"),
+	@PrimaryKeyJoinColumn(name="rev", referencedColumnName="rev")
+})
+@SecondaryTable(name="producttype", pkJoinColumns={
+		@PrimaryKeyJoinColumn(name="type_id", referencedColumnName="type_id")
+})
 public class Category {
 
+	private Category _parent;
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	public Category getParent() {
+		return _parent;
+	}
+
+	@SuppressWarnings("unused")
+	private void setParent(Category parent) {
+		_parent = parent;
+	}
 }
