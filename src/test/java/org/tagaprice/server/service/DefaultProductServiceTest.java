f4640d5a5fa30c14f0b5dfa1abfa1f043dd0a266
forste <s.fortelny@gmx.at>
2010-12-27 15:01:33 +0100

    Fixed automatic application configuration retrieval.

--- src/test/java/org/tagaprice/server/service/DefaultProductServiceTest.java
+++ src/test/java/org/tagaprice/server/service/DefaultProductServiceTest.java
@@ -6,12 +6,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.tagaprice.core.entities.Product;
+import org.tagaprice.server.dao.interfaces.IProductDAO;
+
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
 
 @ContextConfiguration
-public class DefaultProductServiceTest  extends AbstractTransactionalJUnit4SpringContextTests {
-	//	private DefaultProductService _productManagement;
-	//	private IProductDAO _productDaoMock;
+public class DefaultProductServiceTest  extends AbstractJUnit4SpringContextTests {
+	private DefaultProductService _productManagement;
+	private IProductDAO _productDaoMock;
 
 	@BeforeClass
 	public static void setUpBefore() throws Exception {
@@ -19,9 +27,9 @@ public class DefaultProductServiceTest  extends AbstractTransactionalJUnit4Sprin
 
 	@Before
 	public void setUp() throws Exception {
-		//		_productManagement = applicationContext.getBean("defaultProductManagement", DefaultProductService.class); //maybe replace this with autowire
-		//		_productDaoMock = mock(IProductDAO.class);
-		//		_productManagement.setProductDAO(_productDaoMock); //TODO replace this by dependency injection via autowire, how to inject mock ? see http://stackoverflow.com/questions/2457239/injecting-mockito-mocks-into-a-spring-bean for help
+		_productManagement = applicationContext.getBean("defaultProductManagement", DefaultProductService.class); //maybe replace this with autowire
+		_productDaoMock = mock(IProductDAO.class);
+		_productManagement.setProductDAO(_productDaoMock); //TODO replace this by dependency injection via autowire, how to inject mock ? see http://stackoverflow.com/questions/2457239/injecting-mockito-mocks-into-a-spring-bean for help
 	}
 
 	@After
@@ -29,14 +37,14 @@ public class DefaultProductServiceTest  extends AbstractTransactionalJUnit4Sprin
 
 	@Test
 	public void saveNewProduct_shouldReturnProductWithActualProductRevision() {
-		//		Product productToSave = new Product().setId(null).setTitle("productTitle");
-		//		Product expected = new Product().setId((long) 1).setTitle("productTitle");
+		Product productToSave = null; //new Product().setId(null).setTitle("productTitle");
+		Product expected = null; //new Product().setId((long) 1).setTitle("productTitle");
 
-		//		when(_productDaoMock.save(productToSave)).thenReturn(expected);
-		//
-		//
-		//		Product actual = _productManagement.save(productToSave);
-		//
-		//		assertThat(actual, equalTo(expected));
+		when(_productDaoMock.save(productToSave)).thenReturn(expected);
+
+
+		Product actual = _productManagement.save(productToSave);
+
+		assertThat(actual, equalTo(expected));
 	}
 }
