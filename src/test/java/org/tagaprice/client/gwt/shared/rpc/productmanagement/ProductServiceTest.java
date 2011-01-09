3fcb7c84ee9807d2d9da1c67dbca670c28386c2b
Helga Weik <helga@weik.at>
2011-01-09 20:08:39 +0100

    Updated somme comments and two methods in the class ProductServiceTest

--- src/test/java/org/tagaprice/client/gwt/shared/rpc/productmanagement/ProductServiceTest.java
+++ src/test/java/org/tagaprice/client/gwt/shared/rpc/productmanagement/ProductServiceTest.java
@@ -14,9 +14,55 @@ public class ProductServiceTest {
 	static IProduct updatedProduct;
 	static RevisionId updatedRevisionId;
 
+	static IProduct myTestProduct;
+	static RevisionId myTestProductRevisionId;
+	static IProduct myUpdatedTestProduct;
+	static RevisionId myUpdatedTestProductRevisionId;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		// these objects are mocked...
+		/*
+		 * Create 2 Products that are initially saved into the ProductService
+		 */
 
+		/*
+		 * A first Mock that imitates a new Product with the special productId = 0 and revisionId = 0.
+		 */
+		ProductServiceTest.initialProduct = Mockito.mock(IProduct.class);
+		ProductServiceTest.initialRevisionId = Mockito.mock(RevisionId.class);
+		// Add responses
+		Mockito.when(ProductServiceTest.initialRevisionId.getId()).thenReturn(0L);
+		Mockito.when(ProductServiceTest.initialRevisionId.getRevision()).thenReturn(0L);
+		Mockito.when(ProductServiceTest.initialProduct.getRevisionId())
+		.thenReturn(ProductServiceTest.initialRevisionId);
+		Mockito.when(ProductServiceTest.initialProduct.getTitle()).thenReturn("TestTitle");
+		// Add expectations
+
+		/*
+		 * A second Mock that imitates the first product after the initial save.
+		 * We assumed, that the productId of the new product is 1.
+		 */
+		ProductServiceTest.updatedProduct = Mockito.mock(IProduct.class);
+		ProductServiceTest.updatedRevisionId = Mockito.mock(RevisionId.class);
+		// Add responses
+		Mockito.when(ProductServiceTest.updatedRevisionId.getId()).thenReturn(1L);
+		Mockito.when(ProductServiceTest.updatedRevisionId.getRevision()).thenReturn(1L);
+		Mockito.when(ProductServiceTest.updatedProduct.getRevisionId())
+		.thenReturn(ProductServiceTest.updatedRevisionId);
+		Mockito.when(ProductServiceTest.updatedProduct.getTitle()).thenReturn("NewTestTitle");
+
+		ProductServiceTest.myTestProduct = Mockito.mock(IProduct.class);
+		//The initial Revision is always the same
+		ProductServiceTest.myTestProductRevisionId = ProductServiceTest.initialRevisionId;
+
+		Mockito.when(ProductServiceTest.myTestProduct.getTitle()).thenReturn("TheNewestProduct");
+		Mockito.when(ProductServiceTest.myTestProduct.getRevisionId()).thenReturn(ProductServiceTest.myTestProductRevisionId);
+
+		ProductServiceTest.myUpdatedTestProduct = Mockito.mock(IProduct.class);
+		ProductServiceTest.myUpdatedTestProductRevisionId = Mockito.mock(RevisionId.class);
+		Mockito.when(ProductServiceTest.myTestProduct.getTitle()).thenReturn("TheSecondNewestProduct");
+		Mockito.when(ProductServiceTest.myUpdatedTestProduct.getRevisionId()).thenReturn(ProductServiceTest.myUpdatedTestProductRevisionId);
 	}
 
 	@AfterClass
@@ -25,32 +71,17 @@ public class ProductServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		//class under test... must be replaced by implementation
+		// class under test... must be replaced by implementation
 		ProductServiceTest.productService = Mockito.mock(IProductService.class);
 
-		//these objects are mocked...
-		ProductServiceTest.initialProduct = Mockito.mock(IProduct.class);
-		//Add responses
-		Mockito.when(ProductServiceTest.initialRevisionId.getId()).thenReturn(0L);
-		Mockito.when(ProductServiceTest.initialRevisionId.getRevision()).thenReturn(0L);
-		Mockito.when(ProductServiceTest.initialProduct.getRevisionId()).thenReturn(ProductServiceTest.initialRevisionId);
-		Mockito.when(ProductServiceTest.initialProduct.getTitle()).thenReturn("TestTitle");
-		//Add expectations
-		ProductServiceTest.updatedProduct = Mockito.mock(IProduct.class);
-		//Add responses
-
-		//We assume this!!!
-		Mockito.when(ProductServiceTest.updatedRevisionId.getId()).thenReturn(1L);
-		Mockito.when(ProductServiceTest.updatedRevisionId.getRevision()).thenReturn(1L);
-		Mockito.when(ProductServiceTest.updatedProduct.getRevisionId()).thenReturn(ProductServiceTest.updatedRevisionId);
-		Mockito.when(ProductServiceTest.updatedProduct.getTitle()).thenReturn("NewTestTitle");
-		//Add expectations
-
+		/*
+		 * We store the two Objects into the productService.
+		 */
 		ProductServiceTest.productService.saveProduct(ProductServiceTest.initialProduct);
 		ProductServiceTest.productService.saveProduct(ProductServiceTest.updatedProduct);
-
-		//Now we have 1 product saved with 2 revisions
-
+		/*
+		 * Now we have 1 product saved with 2 revisions in the ProductService.
+		 */
 	}
 
 	@After
@@ -58,31 +89,32 @@ public class ProductServiceTest {
 	}
 
 	/**
-	 * A product with id=0 gets a new id and a initial revisionId.
+	 * TESTCASE: A new product is saved.
+	 * A product with id=0 gets a new id and an initial revisionId.
 	 * The first product will get id=1, and RevisionId=1.
 	 * Productname
 	 */
 	@Test
 	public void saveNewProduct_shouldReturnNewProduct() {
-		IProduct myTestProduct = Mockito.mock(IProduct.class);
-		IProduct mySavedProduct = ProductServiceTest.productService.saveProduct(myTestProduct);
 
+		IProduct mySavedProduct = ProductServiceTest.productService.saveProduct(ProductServiceTest.myTestProduct);
+		// We assume that the new productId is 2 because we already have stored one product (id 1 is used).
 		Assert.assertTrue(2L == mySavedProduct.getRevisionId().getId());
-		Assert.assertTrue(2L == mySavedProduct.getRevisionId().getRevision());
+		Assert.assertTrue(1L == mySavedProduct.getRevisionId().getRevision());
 
 	}
 
 	/**
+	 * TESTCASE: An existing product is updated.
 	 * UPDATE: the existing product gets a new revisionId incremented by one.
 	 */
 	@Test
 	public void saveAndUpdateProduct_shouldReturnNewRevision() {
+		ProductServiceTest.productService.saveProduct(ProductServiceTest.myTestProduct);
+		IProduct mySavedProduct = ProductServiceTest.productService.saveProduct(ProductServiceTest.myUpdatedTestProduct);
 
-		// the title of an existing product is changed, the same product gets a new revision and a new id
-		ProductServiceTest.updatedProduct.setTitle("TestProduct2");
-		ProductServiceTest.productService.saveProduct(ProductServiceTest.updatedProduct);
-
-		Assert.assertTrue(3L == ProductServiceTest.updatedProduct.getRevisionId().getRevision());
+		Assert.assertTrue(2L == mySavedProduct.getRevisionId().getId());
+		Assert.assertTrue(2L == mySavedProduct.getRevisionId().getRevision());
 
 	}
 
@@ -93,8 +125,6 @@ public class ProductServiceTest {
 	public void saveAndUpdateProduct_shouldThrowException() {
 
 	}
-
-
 
 
 
