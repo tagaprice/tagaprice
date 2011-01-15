55cf92269749a1954685d5c35ce46c3716a2e7a4
haja <h.jagenteufel@gmail.com>
2011-01-15 17:54:55 +0100

    removed unnecessary comments/TODOs

--- src/main/java/org/tagaprice/server/dao/hibernate/HibernateProductDAO.java
+++ src/main/java/org/tagaprice/server/dao/hibernate/HibernateProductDAO.java
@@ -9,10 +9,8 @@ import org.hibernate.criterion.Projections;
 import org.tagaprice.core.entities.Product;
 import org.tagaprice.server.dao.interfaces.IProductDAO;
 
-/** TODO proper setup through spring beans config */
 public class HibernateProductDAO implements IProductDAO {
 
-	/** TODO @Autowired */
 	private SessionFactory _sessionFactory;
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
@@ -20,23 +18,14 @@ public class HibernateProductDAO implements IProductDAO {
 	}
 
 	@Override
-	/** TODO remove @Transactional from this method! if removed, tests aren't working. find a way to setup tests correctly */
 	public Product save(Product product) {
 		Session session = _sessionFactory.getCurrentSession();
-		//		session.beginTransaction();
 		session.save(product);
-		//		session.getTransaction().commit();
 		return product;
 	}
 
 	@Override
 	public Product getById(Long id) {
-		//		ProductRevision key = new ProductRevision();
-		//
-		//		ReflectionTestUtils.invokeSetterMethod(key, "setId", id);
-		//		ReflectionTestUtils.invokeSetterMethod(key, "setRevisionNumber",revision);
-
-		//		_sessionFactory.getCurrentSession().get
 		return (Product) _sessionFactory.getCurrentSession().load(Product.class, id);
 	}
 
