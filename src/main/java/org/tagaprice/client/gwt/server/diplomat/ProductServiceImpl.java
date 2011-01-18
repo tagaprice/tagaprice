3cf1b95606625192d7b5873a712d23a688285fff
forste <s.fortelny@gmx.at>
2011-01-18 18:55:59 +0100

    Spring integration for gwt-ProductServiceImpl, logging for main

--- src/main/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImpl.java
+++ src/main/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImpl.java
@@ -2,11 +2,17 @@ package org.tagaprice.client.gwt.server.diplomat;
 
 import java.util.*;
 
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.tagaprice.client.gwt.shared.entities.*;
 import org.tagaprice.client.gwt.shared.entities.dump.*;
 import org.tagaprice.client.gwt.shared.entities.dump.Unit;
 import org.tagaprice.client.gwt.shared.entities.productmanagement.*;
-import org.tagaprice.client.gwt.shared.logging.*;
 import org.tagaprice.client.gwt.shared.rpc.productmanagement.IProductService;
 import org.tagaprice.core.api.ServerException;
 import org.tagaprice.core.entities.*;
@@ -22,7 +28,6 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  */
 public class ProductServiceImpl extends RemoteServiceServlet implements
 IProductService {
-
 	private org.tagaprice.core.api.IProductService coreService;
 	private static Locale defaultLocale = new Locale(1, "de", "de");
 	private static Account defaultAccount = new Account(1L, "love@you.org", "super", new Date());
@@ -31,15 +36,41 @@ IProductService {
 	 * 
 	 */
 	private static final long serialVersionUID = 1733780607553359495L;
-	MyLogger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
+	Logger _log = LoggerFactory.getLogger(ProductServiceImpl.class);
 
+	public ProductServiceImpl() throws Exception {
+		_log.debug("Starting GWT-ProductService");
+		
+		_log.debug("Attempting to load product service from core.api");
+		try {
+			/*
+			 * TODO to avoid static mapping by providing name of context, try something like this. 
+			 * Does not work since this servlet apparently does not have the root application context associated with it.
+			 * 
+			 * WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+			 * applicationContext.getBean("defaultProductService", org.tagaprice.core.api.IProductService.class)
+			 * 
+			 */
+			ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring/beans.xml");
+			coreService = (org.tagaprice.core.api.IProductService) ctx.getBean("defaultProductService");
+		} catch(Exception e) {
+			_log.debug(e.getClass()+": "+e.getMessage());
+			throw e;
+		} finally {
+			_log.debug("Loaded product service successfully.");
+		}
+	}
+	
 	@Override
 	public IProduct getProduct(IRevisionId revionsId) {
+		_log.debug("revisionsId: "+revionsId);
 		return null;
 	}
-
+	
 	@Override
 	public ArrayList<IProduct> getProducts(IProduct searchCriteria) {
+		_log.debug("get products for search criteria");
+		
 		List<Product> list = new ArrayList<Product>();
 		try {
 			list = coreService.getByTitle(searchCriteria.getTitle());
@@ -57,12 +88,14 @@ IProductService {
 
 	@Override
 	public IProduct saveProduct(IProduct product) {
+		_log.debug("product :"+product);
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ArrayList<ICategory> getCategories() {
+		_log.debug("attempting to get available categories");
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -104,6 +137,10 @@ IProductService {
 		IRevisionId revisionId = new RevisionId(id, revision);
 		IProduct productGWT = new org.tagaprice.client.gwt.shared.entities.productmanagement.Product(revisionId, title, category, quantity);
 		return productGWT;
+	}
+	
+	public void setProductService(org.tagaprice.core.api.IProductService productService) {
+		coreService = productService;
 	}
 
 }
