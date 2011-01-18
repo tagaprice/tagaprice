530c71993843a0ab522e323627f115ab3223444b
Martin Weik <martin@weik.at>
2011-01-18 14:17:18 +0100

    created stubs for conversion between server and gwt

--- src/main/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImpl.java
+++ src/main/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImpl.java
@@ -7,6 +7,10 @@ import org.tagaprice.client.gwt.shared.entities.dump.*;
 import org.tagaprice.client.gwt.shared.entities.productmanagement.*;
 import org.tagaprice.client.gwt.shared.logging.*;
 import org.tagaprice.client.gwt.shared.rpc.productmanagement.IProductService;
+import org.tagaprice.core.entities.*;
+import org.tagaprice.core.entities.Category;
+import org.tagaprice.core.entities.Locale;
+import org.tagaprice.core.entities.Product;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 /**
@@ -17,6 +21,10 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class ProductServiceImpl extends RemoteServiceServlet implements
 IProductService {
 
+	private org.tagaprice.core.api.IProductService coreService;
+	private static Locale defaultLocale = new Locale(1, "de", "de");
+	private static Account defaultAccount = new Account(1L, "love@you.org", "super", new Date());
+
 	/**
 	 * 
 	 */
@@ -25,7 +33,6 @@ IProductService {
 
 	@Override
 	public IProduct getProduct(IRevisionId revionsId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -45,6 +52,44 @@ IProductService {
 	public ArrayList<ICategory> getCategories() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private org.tagaprice.core.entities.Product convertProductToCore(final IProduct productGWT) {
+		//Default values for new product...
+		Long productId = 0L;
+		Integer revisionNumber = 1;
+		String title = productGWT.getTitle();
+		Date date = new Date();
+		Category category = new Category(null, productGWT.getCategory().getTitle(), null, new Date(), ProductServiceImpl.defaultAccount);
+		//If product allready exists...
+		if(productGWT.getRevisionId() != null && productGWT.getRevisionId().getId() != null && productGWT.getRevisionId().getId() != 0L) {
+			//productId =
+		}
+		ProductRevision revision = new ProductRevision(productId, revisionNumber, title, date, ProductServiceImpl.defaultAccount, null, null, category, "");
+		Product productCore = new Product(productGWT.getRevisionId().getId(), ProductServiceImpl.defaultLocale, null);
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param productCore
+	 * @param revision when 0, then the latest revision is returned.
+	 * @return
+	 */
+	private IProduct convertProductToGWT(final Product productCore, int revisionToGet) {
+		//these are allways existing products!!!
+		//get the data from the latest revision
+		long id = 0;
+		long revision = 0;
+		String title = "";
+		ICategory category = null;
+		IQuantity quantity = null;
+
+		IRevisionId revisionId = new RevisionId(id, revision);
+		IProduct productGWT = new org.tagaprice.client.gwt.shared.entities.productmanagement.Product(title, category, quantity);
+
+
+		return productGWT;
 	}
 
 }
---
+++ src/test/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImplTest.java
@@ -0,0 +1,34 @@
+package org.tagaprice.client.gwt.server.diplomat;
+
+
+import org.junit.*;
+
+public class ProductServiceImplTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConvertProductToCore() throws Exception {
+		Assert.fail("not implemented");
+	}
+
+	@Test
+	public void testConvertProductToGWT() throws Exception {
+		Assert.fail("not implemented");
+	}
+
+}
