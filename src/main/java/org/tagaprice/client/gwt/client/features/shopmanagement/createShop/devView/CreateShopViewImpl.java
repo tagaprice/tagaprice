863a76a58797b5b46b048dc6bf371cdbbf4a1d94
Jürgen Brandstetter <juergen@tagaprice.org>
2011-01-19 01:14:51 +0100

    Add xml.-style file for shop

--- src/main/java/org/tagaprice/client/gwt/client/features/shopmanagement/createShop/devView/CreateShopViewImpl.java
+++ src/main/java/org/tagaprice/client/gwt/client/features/shopmanagement/createShop/devView/CreateShopViewImpl.java
@@ -2,20 +2,34 @@ package org.tagaprice.client.gwt.client.features.shopmanagement.createShop.devVi
 
 import org.tagaprice.client.gwt.client.features.shopmanagement.createShop.ICreateShopView;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class CreateShopViewImpl extends Composite implements ICreateShopView {
+	interface CreateShopViewImpleUiBinder extends
+	UiBinder<Widget, CreateShopViewImpl> {
+	}
+
+	private static CreateShopViewImpleUiBinder uiBinder = GWT
+	.create(CreateShopViewImpleUiBinder.class);
 
 	private Presenter _presenter;
 
+	VerticalPanel vePa = new VerticalPanel();
+
 	public CreateShopViewImpl() {
-		initWidget(new Label("create layout here"));
+		initWidget(CreateShopViewImpl.uiBinder.createAndBindUi(this));
+
+
+
 	}
 
 	@Override
 	public void setPresenter(Presenter presenter) {
-		_presenter=presenter;
+		_presenter = presenter;
 	}
 
 }
