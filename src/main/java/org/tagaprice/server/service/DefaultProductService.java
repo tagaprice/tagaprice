28024c2b1edabe973de4b37e2b2124956d1d0c5e
haja <h.jagenteufel@gmail.com>
2011-01-22 22:41:16 +0100

    dirty workaround for hibernate lazyness

--- src/main/java/org/tagaprice/server/service/DefaultProductService.java
+++ src/main/java/org/tagaprice/server/service/DefaultProductService.java
@@ -21,8 +21,8 @@ public class DefaultProductService implements IProductService {
 	private IProductDAO _productDao;
 	private Logger _log = LoggerFactory.getLogger(DefaultProductService.class);
 	private IProductRevisionDAO _productRevisionDao;
-	
-	
+
+
 
 	public DefaultProductService() {
 		_log.debug("Creating defaultproductservice");
@@ -84,21 +84,28 @@ public class DefaultProductService implements IProductService {
 		List<ProductRevision> revisions = _productRevisionDao.getByTitle(title);
 		_log.debug("number of revisions found:" + revisions.size());
 
-		
+
 		HashSet<Long> ids = new HashSet<Long>();
 		for(ProductRevision revision : revisions) {
 			ids.add(revision.getId());
 		}
-		
+
 		_log.debug("number of different product ids found:" + ids.size());
-		
+
 
 		List<Product> products = new ArrayList<Product>();
 
 		for(Long id : ids) {
 			products.add(_productDao.getById(id));
 		}
-		
+
+		// WORKAROUND for hibernate being lazy
+		for(Product p : products) {
+			p.getRevisions();
+			p.getCurrentRevision();
+			p.getId();
+		}
+
 		_log.debug("number of product found:" + ids.size());
 
 		return products;
