3e648fb2e992d1f86813f8f9af39103a22b93ee9
haja <h.jagenteufel@gmail.com>
2011-01-24 13:08:52 +0100

    added save to HibernateAccountDAO + test

--- src/test/java/org/tagaprice/server/dao/interfaces/AbstractAccountDaoTest.java
+++ src/test/java/org/tagaprice/server/dao/interfaces/AbstractAccountDaoTest.java
@@ -2,8 +2,8 @@ package org.tagaprice.server.dao.interfaces;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
 import org.dbunit.dataset.IDataSet;
+import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.tagaprice.core.api.WrongEmailOrPasswordException;
 import org.tagaprice.core.entities.Account;
 import org.tagaprice.server.boot.dbinit.IDbTestInitializer;
+import org.tagaprice.server.dao.helper.DbSaveAssertUtility;
 import org.tagaprice.server.dao.helper.HibernateSaveEntityCreator;
 
 @ContextConfiguration
@@ -24,6 +25,7 @@ public class AbstractAccountDaoTest extends AbstractTransactionalJUnit4SpringCon
 	private Logger _log = LoggerFactory.getLogger(AbstractProductDaoTest.class);
 	@SuppressWarnings("unused")
 	private IDataSet _currentDataSet;
+	private SessionFactory _sessionFactory;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -39,11 +41,14 @@ public class AbstractAccountDaoTest extends AbstractTransactionalJUnit4SpringCon
 		_currentDataSet = _dbInitializer.fillTables();
 
 		_accountDao = applicationContext.getBean("accountDao", IAccountDAO.class);
+
+		_sessionFactory = applicationContext.getBean("sessionFactory", SessionFactory.class);
+		DbSaveAssertUtility.setSimpleJdbcTemplate(super.simpleJdbcTemplate);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		_dbInitializer.resetTables();
+		//_dbInitializer.resetTables(); //this causes infinite loop on save test
 	}
 
 	@Test(expected=WrongEmailOrPasswordException.class)
@@ -64,6 +69,24 @@ public class AbstractAccountDaoTest extends AbstractTransactionalJUnit4SpringCon
 		Account expected = HibernateSaveEntityCreator.createAccount(1L, email, password);
 
 		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void saveNewAccount_shouldPersist() throws Exception {
+		String email = "userX@mymail.com";
+		String password = "123";
+
+		Account accountToSave = HibernateSaveEntityCreator.createAccount(null, email, password); // new accounts don't have any id
+
+		Account actual = _accountDao.save(accountToSave);
+
+		Long nextAccountId = 3L; // TODO these settings should be in EntityCreator
+		Account expected = HibernateSaveEntityCreator.createAccount(nextAccountId, email, password);
+
+		assertThat(actual, is(expected));
+
+		_sessionFactory.getCurrentSession().flush();
+		DbSaveAssertUtility.assertEntitySaved(actual);
 	}
 
 
