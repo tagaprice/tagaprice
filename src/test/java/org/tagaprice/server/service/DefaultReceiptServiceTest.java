66e3a09942b44b449870caca050f9d41c8726465
haja <h.jagenteufel@gmail.com>
2011-01-24 22:30:10 +0100

    refactored ReceiptServiceTest to ReceiptServiceTestTemplate and added ReceiptManagementTest

--- src/test/java/org/tagaprice/server/service/DefaultReceiptServiceTest.java
+++ src/test/java/org/tagaprice/server/service/DefaultReceiptServiceTest.java
@@ -5,9 +5,11 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Before;
 import org.springframework.test.context.ContextConfiguration;
+import org.tagaprice.core.entities.Receipt;
 import org.tagaprice.core.entities.ReceiptEntry;
 import org.tagaprice.server.dao.interfaces.IReceiptDAO;
 import org.tagaprice.server.service.templates.ReceiptServiceTestTemplate;
@@ -19,7 +21,7 @@ public class DefaultReceiptServiceTest extends ReceiptServiceTestTemplate {
 	public void setUp() throws Exception {
 		_receiptService = applicationContext.getBean("defaultReceiptService", DefaultReceiptService.class);
 		_receiptDao = mock(IReceiptDAO.class);
-		_receiptService.setReceiptDAO(_receiptDao);
+		((DefaultReceiptService) _receiptService).setReceiptDAO(_receiptDao);
 	}
 
 	@Override
@@ -30,5 +32,29 @@ public class DefaultReceiptServiceTest extends ReceiptServiceTestTemplate {
 	@Override
 	protected void getReceiptEntriesByProductIdAndRev_shouldReturnEntriesAsGivenByDaoButOnlyUnique_DAO_SETUP(Long productId, Integer productRevision, List<ReceiptEntry> entries) {
 		when(_receiptDao.getReceiptEntriesByProductIdAndRev(productId, productRevision)).thenReturn(entries);
+	}
+
+	@Override
+	protected void saveEmptyReceipt_shouldSaveReceipt_DB_ASSERTS(Receipt receiptToSave) {
+	}
+
+	@Override
+	protected void saveEmptyReceipt_shouldSaveReceipt_DAO_SETUP(Receipt receiptToSave) {
+		when(_receiptDao.save(receiptToSave)).thenReturn(receiptToSave);
+	}
+
+	@Override
+	protected void saveReceiptWithReceiptEntries_shouldSaveReceipt_DAO_SETUP(Receipt receipt) 	{
+		when(_receiptDao.save(receipt)).thenReturn(receipt);
+	}
+
+
+	//
+	// methods not needed
+	//
+
+	@Override
+	protected void saveReceiptWithReceiptEntries_shouldSaveReceipt_DB_ASSERTS(Receipt receiptToSave,
+			Set<ReceiptEntry> receiptEntries) {
 	}
 }
