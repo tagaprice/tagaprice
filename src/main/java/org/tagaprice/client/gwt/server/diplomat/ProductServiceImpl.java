9fbb2aadd4431b4c6f69ab185aef11e0c4658512
Martin Weik <martin@weik.at>
2011-01-24 17:56:15 +0100

    removed static product...

--- src/main/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImpl.java
+++ src/main/java/org/tagaprice/client/gwt/server/diplomat/ProductServiceImpl.java
@@ -114,20 +114,9 @@ public class ProductServiceImpl extends RemoteServiceServlet implements IProduct
 		_log.debug("product :"+product);
 
 		try {
-			Product productCore;
-			String newProductTitle = "newProduct";
-			Unit newProductUnit = Unit.kg;
-			Double newProductAmount = 2.3;
-			String newProductImageURL = "";
-			Category category = new Category(null, "testcat", null, new Date(), ProductServiceImpl.defaultCoreAccount);
+			Product productCore = this.convertProductToCore(product);
 
-			ProductRevision newProductRevision = new ProductRevision(null, 1, newProductTitle, new Date(), ProductServiceImpl.defaultCoreAccount, newProductUnit, newProductAmount, category , newProductImageURL);
-			java.util.HashSet<ProductRevision> revisions = new java.util.HashSet<ProductRevision>();
-			revisions.add(newProductRevision);
-
-			productCore = new Product(null, ProductServiceImpl.defaultCoreLocale, revisions);
-
-			this._coreProductService.save(productCore);
+			productCore = this._coreProductService.save(productCore);
 
 			return convertProductToGWT(productCore, 0);
 		} catch (OutdatedRevisionException e) {
