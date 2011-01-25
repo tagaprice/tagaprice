b7920744e5b68817e9202cee68c915d141235fa0
forste <s.fortelny@gmx.at>
2011-01-25 13:23:21 +0100

    Trying to get sql generator for product ids to work

--- src/main/java/org/tagaprice/core/entities/ProductRevision.java
+++ src/main/java/org/tagaprice/core/entities/ProductRevision.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -109,6 +111,7 @@ public class ProductRevision implements Serializable {
 	}
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ent_id")
 	public Long getId() {
 		return _id;
