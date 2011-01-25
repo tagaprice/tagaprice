5a4c34c0ea5bf5ccd83ae2cc1a446df0637b0659
Martin Weik <martin@weik.at>
2011-01-25 15:55:19 +0100

    created methods in ReceiptService Interfaces

--- src/main/java/org/tagaprice/client/gwt/shared/rpc/receiptManagement/IReceiptServiceAsync.java
+++ src/main/java/org/tagaprice/client/gwt/shared/rpc/receiptManagement/IReceiptServiceAsync.java
@@ -1,5 +1,17 @@
 package org.tagaprice.client.gwt.shared.rpc.receiptManagement;
 
+import java.util.ArrayList;
+
+import org.tagaprice.client.gwt.shared.entities.receiptManagement.*;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
 public interface IReceiptServiceAsync {
+
+	void getReceipt(long receiptid, AsyncCallback<IReceipt> callback);
+
+	void getReceiptEntriesByProductId(long productid, AsyncCallback<ArrayList<ReceiptEntry>> callback);
+
+	void save(IReceipt receipt, AsyncCallback<IReceipt> callback);
 
 }
