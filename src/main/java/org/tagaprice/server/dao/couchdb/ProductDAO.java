e571ff6fff3d6f594225d367be2f1141f4bfbd56
Manuel Reithuber <git@manuel.reithuber.net>
2011-04-07 18:45:34 +0200

    fixed some type conversion warnings in ProductDAO and CategoryDAO

--- src/main/java/org/tagaprice/server/dao/couchdb/ProductDAO.java
+++ src/main/java/org/tagaprice/server/dao/couchdb/ProductDAO.java
@@ -2,13 +2,11 @@ package org.tagaprice.server.dao.couchdb;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.jcouchdb.document.ValueRow;
 import org.jcouchdb.document.ViewResult;
 import org.tagaprice.server.dao.IProductDAO;
 import org.tagaprice.shared.entities.RevisionId;
-import org.tagaprice.shared.entities.dump.ICategory;
 import org.tagaprice.shared.entities.productmanagement.IProduct;
 import org.tagaprice.shared.entities.productmanagement.Product;
 
@@ -20,11 +18,11 @@ public class ProductDAO extends DAOClass<IProduct> implements IProductDAO {
 	@Override
 	public List<IProduct> find(IProduct searchPattern) {
 		/// TODO implement actual searching here
-		ViewResult<Map> result = m_db.listDocuments(null, null);
+		ViewResult<?> result = m_db.listDocuments(null, null);
 		List<IProduct> rc = new ArrayList<IProduct>();
 		
 		System.out.println("CatList:");
-		for (ValueRow<Map> row: result.getRows()) {
+		for (ValueRow<?> row: result.getRows()) {
 			IProduct product = get(new RevisionId(row.getId()));
 			rc.add(product);
 		}
