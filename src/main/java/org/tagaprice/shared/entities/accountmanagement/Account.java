f12d663fe7e771e95271c9233afddf42093c9a39
Manuel Reithuber <git@manuel.reithuber.net>
2011-04-08 13:09:38 +0200

    removed double User class

--- src/main/java/org/tagaprice/shared/entities/accountmanagement/Account.java
+++ src/main/java/org/tagaprice/shared/entities/accountmanagement/Account.java
@@ -1,8 +1,10 @@
-package org.tagaprice.shared.entities;
+package org.tagaprice.shared.entities.accountmanagement;
 
 import org.svenson.JSONProperty;
 
 public class Account extends User {
+	private static final long serialVersionUID = 1L;
+
 	private String m_mail;
 	
 	@JSONProperty(value="mail")
