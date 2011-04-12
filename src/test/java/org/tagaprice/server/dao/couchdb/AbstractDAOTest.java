6185e85270d547258c9dfcfc752b40d05b5eb448
Manuel Reithuber <git@manuel.reithuber.net>
2011-04-12 23:17:35 +0200

    Created class AbstractDAOTest that acts as a base class for other CouchDB unit test classes and provides some setup/cleanup methods

---
+++ src/test/java/org/tagaprice/server/dao/couchdb/AbstractDAOTest.java
@@ -0,0 +1,38 @@
+package org.tagaprice.server.dao.couchdb;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Base class for CouchDB DAO unit tests that provides automatic DB cleanup
+ */
+public class AbstractDAOTest {
+	Set<DAOClass<?>> m_daoClasses = new TreeSet<DAOClass<?>>();
+	
+	/**
+	 * Add a DAO class to the automatic cleanup list
+	 * @param daoClass DAO class to add to the list
+	 */
+	protected void addDAOClass(DAOClass<?> daoClass) {
+		m_daoClasses.add(daoClass); 
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		for (DAOClass<?> daoClass: m_daoClasses) {
+			if (daoClass.hasDB()) daoClass.deleteDB();
+			daoClass.createDB();
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		for (DAOClass<?> daoClass: m_daoClasses) {
+			daoClass.deleteDB();
+		}
+	}
+
+}
