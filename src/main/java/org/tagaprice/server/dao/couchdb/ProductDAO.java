3de1b6c08aad685fd14e605abc303f029bf4ec9c
Manuel Reithuber <git@manuel.reithuber.net>
2011-04-14 01:20:39 +0200

    implemented automatic injection of full blown Unit objects in ProductDAO

--- src/main/java/org/tagaprice/server/dao/couchdb/ProductDAO.java
+++ src/main/java/org/tagaprice/server/dao/couchdb/ProductDAO.java
@@ -8,14 +8,17 @@ import org.jcouchdb.document.ViewResult;
 import org.tagaprice.server.dao.ICategoryDAO;
 import org.tagaprice.server.dao.IDaoFactory;
 import org.tagaprice.server.dao.IProductDAO;
+import org.tagaprice.server.dao.IUnitDAO;
 import org.tagaprice.shared.entities.productmanagement.Product;
 
 public class ProductDAO extends DAOClass<Product> implements IProductDAO {
 	ICategoryDAO m_categoryDAO;
+	IUnitDAO m_unitDAO;
 	
 	public ProductDAO(IDaoFactory daoFactory, String dbPrefix) {
 		super(dbPrefix, Product.class, "product");
 		m_categoryDAO = daoFactory.getCategoryDAO();
+		m_unitDAO = daoFactory.getUnitDAO();
 	}
 
 	@Override
@@ -23,6 +26,10 @@ public class ProductDAO extends DAOClass<Product> implements IProductDAO {
 		Product rc = super.get(id, revision);
 		if (rc.getCategoryId() != null) {
 			rc.setCategory(m_categoryDAO.get(rc.getCategoryId()));
+		}
+		
+		if (rc.getUnitId() != null) {
+			rc.setUnit(m_unitDAO.get(rc.getUnitId()));
 		}
 		return rc;
 	}
