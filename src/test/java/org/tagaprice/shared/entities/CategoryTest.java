631bb1816c421cd547ee7cc3fb2e6cdec29bdd34
Jürgen Brandstetter <juergen@tagaprice.org>
2011-04-23 17:56:08 +0200

    Implemented Simple CategoryDTO test

--- src/test/java/org/tagaprice/shared/entities/CategoryTest.java
+++ src/test/java/org/tagaprice/shared/entities/CategoryTest.java
@@ -4,66 +4,87 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.tagaprice.shared.entities.categorymanagement.Category;
 
 public class CategoryTest {
 
+	Category emptyCategory;
+	Category newCategory;
+	Category updateCategory;
+	Category setterCategory;
+
 	@Before
 	public void setUp() throws Exception {
+		emptyCategory = new Category();
+		newCategory = new Category("title", new Category("parent", null));
+		updateCategory = new Category("1", "2", "update", new Category("updateparent", null));
+		setterCategory = new Category();
 	}
 
 	@Test
 	public void testCategory() {
-		fail("Not yet implemented");
+		assertNull(emptyCategory.getParentCategory());
+		assertNull(emptyCategory.getTitle());
+		assertNull(emptyCategory.getId());
+		assertNull(emptyCategory.getRevision());
 	}
 
 	@Test
 	public void testCategoryStringCategory() {
-		fail("Not yet implemented");
+		assertEquals(newCategory.getTitle(), "title");
+		assertNotNull(newCategory.getParentCategory());
+		assertEquals(newCategory.getParentCategory().getTitle(), "parent");
+		assertNull(newCategory.getId());
+		assertNull(newCategory.getRevision());
 	}
 
-	@Test
-	public void testCategoryStringStringStringCategory() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testGetParentCategory() {
-		fail("Not yet implemented");
+		assertNotNull(updateCategory.getParentCategory());
+		assertEquals(updateCategory.getParentCategory().getTitle(), "updateparent");
 	}
 
 	@Test
 	public void testSetParentCategory() {
-		fail("Not yet implemented");
+		setterCategory.setParentCategory(new Category("parent", null));
+		assertNotNull(setterCategory.getParentCategory());
+		assertEquals(setterCategory.getParentCategory().getTitle(), "parent");
+		assertNull(setterCategory.getParentCategory().getParentCategory());
+
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		assertEquals(updateCategory.getTitle(), "update");
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail("Not yet implemented");
+		setterCategory.setTitle("title");
+		assertEquals(setterCategory.getTitle(), "title");
 	}
 
 	@Test
 	public void testGetRevision() {
-		fail("Not yet implemented");
+		assertEquals(updateCategory.getRevision(),"2");
 	}
 
 	@Test
 	public void testSetRevision() {
-		fail("Not yet implemented");
+		setterCategory.setRevision("15");
+		assertEquals(setterCategory.getRevision(), "15");
 	}
 
 	@Test
 	public void testGetId() {
-		fail("Not yet implemented");
+		assertEquals(updateCategory.getId(),"1");
 	}
 
 	@Test
 	public void testSetId() {
-		fail("Not yet implemented");
+		setterCategory.setId("9");
+		assertEquals(setterCategory.getId(), "9");
 	}
 
 }
