7bd6aa32a36c850d1e8ed64c639fcb897682c4bd
Jürgen Brandstetter <juergen@tagaprice.org>
2011-04-23 21:24:13 +0200

    Implemented simple DTO tests for Product and Receipt

--- src/test/java/org/tagaprice/shared/entities/ProductTest.java
+++ src/test/java/org/tagaprice/shared/entities/ProductTest.java
@@ -2,113 +2,177 @@ package org.tagaprice.shared.entities;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.tagaprice.shared.entities.categorymanagement.Category;
+import org.tagaprice.shared.entities.productmanagement.Package;
+import org.tagaprice.shared.entities.productmanagement.Product;
 
 public class ProductTest {
 
+	Product emptyProduct;
+	Product newProduct;
+	Product updateProduct;
+	Product setterProduct;
+
 	@Before
 	public void setUp() throws Exception {
+		emptyProduct = new Product();
+		newProduct = new Product("newProduct", new Category("newCategory", null), new Unit("kg"));
+		updateProduct = new Product("1", "2", "updateProduct", new Category("6","7","updateCategory", null), new Unit("4","5","g"));
+		setterProduct = new Product();
 	}
 
 	@Test
 	public void testProduct() {
-		fail("Not yet implemented");
+		assertNull(emptyProduct.getTitle());
+		assertNull(emptyProduct.getId());
+		assertNull(emptyProduct.getRevision());
+		assertNull(emptyProduct.getCategory());
+		assertNull(emptyProduct.getUnit());
+		assertNotNull(emptyProduct.getPackages());
+		assertNull(emptyProduct.getUnitId());
+		assertNotNull(emptyProduct.getPackages());
+		assertEquals(emptyProduct.getPackages().size(), 0);
 	}
 
 	@Test
 	public void testProductStringCategoryUnit() {
-		fail("Not yet implemented");
+		assertEquals(newProduct.getTitle(), "newProduct");
+		assertNull(newProduct.getId());
+		assertNull(newProduct.getRevision());
+		assertNotNull(newProduct.getCategory());
+		assertEquals(newProduct.getCategory().getTitle(), "newCategory");
+		assertNull(newProduct.getCategory().getParentCategory());
+		assertNotNull(newProduct.getUnit());
+		assertEquals(newProduct.getUnit().getTitle(), "kg");
+		assertNotNull(newProduct.getPackages());
+		assertEquals(newProduct.getPackages().size(), 0);
 	}
 
-	@Test
-	public void testProductStringStringStringCategoryUnit() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testSetCategory() {
-		fail("Not yet implemented");
+		assertNull(setterProduct.getCategory());
+		setterProduct.setCategory(new Category("setterCategory", null));
+		assertNotNull(setterProduct.getCategory());
+		assertEquals(setterProduct.getCategory().getTitle(), "setterCategory");
+		assertNull(setterProduct.getCategory().getParentCategory());
 	}
 
 	@Test
 	public void testGetCategory() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getCategory().getTitle(), "updateCategory");
 	}
 
 	@Test
 	public void testGetCategoryId() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getCategoryId(), "6");
 	}
 
 	@Test
 	public void testSetCategoryId() {
-		fail("Not yet implemented");
+		updateProduct.setCategoryId("3");
+		assertEquals(updateProduct.getCategoryId(), "3");
 	}
 
 	@Test
 	public void testAddPackage() {
-		fail("Not yet implemented");
+		assertNotNull(updateProduct.getPackages());
+		assertEquals(updateProduct.getPackages().size(), 0);
+
+		updateProduct.addPackage(new Package(new Quantity(9.9, new Unit("l"))));
+		assertEquals(updateProduct.getPackages().size(), 1);
+		assertEquals(updateProduct.getPackages().get(0).getQuantity().getQuantity(), 9.9, 0.0);
+		assertEquals(updateProduct.getPackages().get(0).getQuantity().getUnit().getTitle(),"l");
+
 	}
 
 	@Test
 	public void testSetPackages() {
-		fail("Not yet implemented");
+		assertNotNull(updateProduct.getPackages());
+		assertEquals(updateProduct.getPackages().size(), 0);
+
+		updateProduct.addPackage(new Package(new Quantity(9.9, new Unit("l"))));
+		assertEquals(updateProduct.getPackages().size(), 1);
+		assertEquals(updateProduct.getPackages().get(0).getQuantity().getQuantity(), 9.9, 0.0);
+		assertEquals(updateProduct.getPackages().get(0).getQuantity().getUnit().getTitle(),"l");
+
+		ArrayList<Package> plist = new ArrayList<Package>();
+		plist.add(new Package(new Quantity(5.5, new Unit("kg"))));
+		plist.add(new Package(new Quantity(4.2, new Unit("ml"))));
+		updateProduct.setPackages(plist);
+		assertEquals(updateProduct.getPackages().size(), 2);
+
+		assertEquals(updateProduct.getPackages().get(0).getQuantity().getQuantity(), 5.5, 0.0);
+		assertEquals(updateProduct.getPackages().get(0).getQuantity().getUnit().getTitle(),"kg");
+
+		assertEquals(updateProduct.getPackages().get(1).getQuantity().getQuantity(), 4.2, 0.0);
+		assertEquals(updateProduct.getPackages().get(1).getQuantity().getUnit().getTitle(),"ml");
+
 	}
 
 	@Test
 	public void testGetPackages() {
-		fail("Not yet implemented");
+		assertNotNull(updateProduct.getPackages());
+		assertEquals(updateProduct.getPackages().size(), 0);
 	}
 
 	@Test
 	public void testSetUnit() {
-		fail("Not yet implemented");
+		updateProduct.setUnit(new Unit("updateTitle"));
+		assertEquals(updateProduct.getUnit().getTitle(), "updateTitle");
 	}
 
 	@Test
 	public void testGetUnit() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getUnit().getTitle(), "g");
 	}
 
 	@Test
 	public void testGetUnitId() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getUnitId(), "4");
 	}
 
 	@Test
 	public void testSetUnitId() {
-		fail("Not yet implemented");
+		updateProduct.setUnitId("5");
+		assertEquals(updateProduct.getUnitId(), "5");
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getTitle(), "updateProduct");
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail("Not yet implemented");
+		updateProduct.setTitle("updateTitle");
+		assertEquals(updateProduct.getTitle(), "updateTitle");
 	}
 
 	@Test
 	public void testGetRevision() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getRevision(), "2");
 	}
 
 	@Test
 	public void testSetRevision() {
-		fail("Not yet implemented");
+		updateProduct.setRevision("8");
+		assertEquals(updateProduct.getRevision(), "8");
 	}
 
 	@Test
 	public void testGetId() {
-		fail("Not yet implemented");
+		assertEquals(updateProduct.getId(), "1");
 	}
 
 	@Test
 	public void testSetId() {
-		fail("Not yet implemented");
+		updateProduct.setId("9");
+		assertEquals(updateProduct.getId(), "9");
 	}
 
 }
--- src/test/java/org/tagaprice/shared/entities/ReceiptTest.java
+++ src/test/java/org/tagaprice/shared/entities/ReceiptTest.java
@@ -2,98 +2,168 @@ package org.tagaprice.shared.entities;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.tagaprice.shared.entities.productmanagement.Package;
+import org.tagaprice.shared.entities.receiptManagement.Currency;
+import org.tagaprice.shared.entities.receiptManagement.Price;
+import org.tagaprice.shared.entities.receiptManagement.Receipt;
+import org.tagaprice.shared.entities.receiptManagement.ReceiptEntry;
+import org.tagaprice.shared.entities.shopmanagement.Shop;
 
 public class ReceiptTest {
 
+	Receipt emptyReceipt;
+	Receipt newReceipt;
+	Receipt updateReceipt;
+	Receipt setterReceipt;
+
 	@Before
 	public void setUp() throws Exception {
+		emptyReceipt = new Receipt();
+		newReceipt = new Receipt("newReceipt", new Date(1303575403414L), new Shop("shop"));
+		updateReceipt = new Receipt("1", "2", "updateReceipt", new Date(1303575403414L), new Shop("3", "4", "updateShop"));
+		setterReceipt = new Receipt();
 	}
 
 	@Test
 	public void testReceipt() {
-		fail("Not yet implemented");
+		assertNull(emptyReceipt.getTitle());
+		assertNull(emptyReceipt.getRevision());
+		assertNull(emptyReceipt.getId());
+		assertNull(emptyReceipt.getDate());
+		assertNotNull(emptyReceipt.getReceiptEntries());
+		assertEquals(emptyReceipt.getReceiptEntries().size(),0);
 	}
 
 	@Test
 	public void testReceiptStringDateShop() {
-		fail("Not yet implemented");
+		assertEquals(newReceipt.getTitle(), "newReceipt");
+		assertNull(newReceipt.getRevision());
+		assertNull(newReceipt.getId());
+		assertEquals(newReceipt.getDate(), new Date(1303575403414L));
+		assertEquals(newReceipt.getShop().getTitle(), "shop");
+		assertNotNull(newReceipt.getReceiptEntries());
+		assertEquals(newReceipt.getReceiptEntries().size(),0);
+
 	}
 
-	@Test
-	public void testReceiptStringStringStringDateShop() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testAddReceiptEntriy() {
-		fail("Not yet implemented");
+		assertEquals(setterReceipt.getReceiptEntries().size(), 0);
+		Package pack = new Package(new Quantity(15.3, new Unit("kg")));
+		ReceiptEntry receipt = new ReceiptEntry(new Price(15, Currency.euro), pack);
+		setterReceipt.addReceiptEntriy(receipt);
+		assertEquals(setterReceipt.getReceiptEntries().size(), 1);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPrice().getPrice(), 15);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPrice().getCurrency(), Currency.euro);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPackage().getQuantity().getQuantity(), 15.3, 0.0);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPackage().getQuantity().getUnit().getTitle(), "kg");
 	}
 
 	@Test
 	public void testGetShop() {
-		fail("Not yet implemented");
+		assertEquals(updateReceipt.getShop().getTitle(), "updateShop");
+		assertEquals(updateReceipt.getShop().getId(), "3");
+		assertEquals(updateReceipt.getShop().getRevision(), "4");
 	}
 
 	@Test
 	public void testGetReceiptEntries() {
-		fail("Not yet implemented");
+		assertNotNull(updateReceipt.getReceiptEntries());
+		assertEquals(updateReceipt.getReceiptEntries().size(),0);
 	}
 
 	@Test
 	public void testGetShopId() {
-		fail("Not yet implemented");
+		assertEquals(updateReceipt.getShopId(), "3");
 	}
 
 	@Test
 	public void testSetShop() {
-		fail("Not yet implemented");
+		setterReceipt.setShop(new Shop("setterShop"));
+		assertEquals(setterReceipt.getShop().getTitle(), "setterShop");
 	}
 
 	@Test
 	public void testSetReceiptEntries() {
-		fail("Not yet implemented");
+		assertEquals(setterReceipt.getReceiptEntries().size(), 0);
+
+		ArrayList<ReceiptEntry> fullList = new ArrayList<ReceiptEntry>();
+		Package pack1 = new Package(new Quantity(15.3, new Unit("kg")));
+		ReceiptEntry receipt1 = new ReceiptEntry(new Price(15, Currency.euro), pack1);
+		fullList.add(receipt1);
+
+		Package pack2 = new Package(new Quantity(16.4, new Unit("g")));
+		ReceiptEntry receipt2 = new ReceiptEntry(new Price(18, Currency.dkk), pack2);
+		fullList.add(receipt2);
+
+		setterReceipt.setReceiptEntries(fullList);
+		assertEquals(setterReceipt.getReceiptEntries().size(), 2);
+
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPrice().getPrice(), 15);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPrice().getCurrency(), Currency.euro);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPackage().getQuantity().getQuantity(), 15.3, 0.0);
+		assertEquals(setterReceipt.getReceiptEntries().get(0).getPackage().getQuantity().getUnit().getTitle(), "kg");
+
+
+		assertEquals(setterReceipt.getReceiptEntries().get(1).getPrice().getPrice(), 18);
+		assertEquals(setterReceipt.getReceiptEntries().get(1).getPrice().getCurrency(), Currency.dkk);
+		assertEquals(setterReceipt.getReceiptEntries().get(1).getPackage().getQuantity().getQuantity(), 16.4, 0.0);
+		assertEquals(setterReceipt.getReceiptEntries().get(1).getPackage().getQuantity().getUnit().getTitle(), "g");
+
+
+		setterReceipt.setReceiptEntries(new ArrayList<ReceiptEntry>());
+		assertEquals(setterReceipt.getReceiptEntries().size(), 0);
+
 	}
 
 	@Test
 	public void testSetDate() {
-		fail("Not yet implemented");
+		setterReceipt.setDate(new Date(1303525403414L));
+		assertEquals(setterReceipt.getDate(), new Date(1303525403414L));
 	}
 
 	@Test
 	public void testGetDate() {
-		fail("Not yet implemented");
+		assertEquals(updateReceipt.getDate(), new Date(1303575403414L));
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		assertEquals(updateReceipt.getTitle(), "updateReceipt");
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail("Not yet implemented");
+		setterReceipt.setTitle("setterTitle");
+		assertEquals(setterReceipt.getTitle(), "setterTitle");
 	}
 
 	@Test
 	public void testGetRevision() {
-		fail("Not yet implemented");
+		assertEquals(updateReceipt.getRevision(), "2");
 	}
 
 	@Test
 	public void testSetRevision() {
-		fail("Not yet implemented");
+		setterReceipt.setRevision("9");
+		assertEquals(setterReceipt.getRevision(), "9");
 	}
 
 	@Test
 	public void testGetId() {
-		fail("Not yet implemented");
+		assertEquals(updateReceipt.getId(), "1");
 	}
 
 	@Test
 	public void testSetId() {
-		fail("Not yet implemented");
+		setterReceipt.setId("8");
+		assertEquals(setterReceipt.getId(), "8");
 	}
 
 }
