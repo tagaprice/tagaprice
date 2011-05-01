71694a962020ea08b197da637c82dda66f4b4422
Manuel Reithuber <git@manuel.reithuber.net>
2011-05-01 19:34:45 +0200

    added CouchDBDaoFactory.getConfiguration()

--- src/main/java/org/tagaprice/server/dao/couchdb/CouchDBDaoFactory.java
+++ src/main/java/org/tagaprice/server/dao/couchdb/CouchDBDaoFactory.java
@@ -1,5 +1,11 @@
 package org.tagaprice.server.dao.couchdb;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.jcouchdb.db.Server;
+import org.jcouchdb.db.ServerImpl;
 import org.tagaprice.server.dao.ICategoryDAO;
 import org.tagaprice.server.dao.IDaoFactory;
 import org.tagaprice.server.dao.IPackageDAO;
@@ -12,6 +18,8 @@ import org.tagaprice.server.dao.IUnitDAO;
  * IDaoFactory implementation providing a CouchDB persistence layer
  */
 public class CouchDBDaoFactory implements IDaoFactory {
+	static Properties m_dbConfig = null;
+	
 	ICategoryDAO m_categoryDAO = null;
 	IPackageDAO m_packageDAO = null;
 	IProductDAO m_productDAO = null;
@@ -19,6 +27,45 @@ public class CouchDBDaoFactory implements IDaoFactory {
 	IShopDAO m_shopDAO = null;
 	IUnitDAO m_unitDAO = null;
 	
+	static Properties getConfiguration() throws IOException {
+		if (m_dbConfig == null) {
+			m_dbConfig = _readProperties("couchdb.properties");
+		}
+		return m_dbConfig;
+	}
+	
+	static Server getServerObject(Properties dbConfig) {
+		return new ServerImpl(
+				dbConfig.getProperty("host", "localhost"),
+				Integer.parseInt(dbConfig.getProperty("port", "5984")),
+				Boolean.parseBoolean(dbConfig.getProperty("ssl", "false"))); // we don't need ssl on localhost
+	}
+	
+	static Server getServerObject() throws IOException {
+		return getServerObject(getConfiguration());
+	}
+	
+	/**
+	 * Read a CouchDB connection configuration file
+	 * @param filename file that should be read
+	 * @return Properties object containing the configuration
+	 * @throws IOException if the config file couldn't be read
+	 */
+	private static Properties _readProperties(String filename) throws IOException {
+		Properties defaults = new Properties();
+		try {
+			InputStream stream = CouchDBDaoFactory.class.getResourceAsStream("/"+filename+".default");
+			if (stream != null) defaults.load(stream);
+		}
+		catch (IOException e) { /* ignore if we can't read the default config as long as we can read the specific one */ }
+		
+		Properties rc = new Properties(defaults);
+		InputStream stream = CouchDBDaoFactory.class.getResourceAsStream("/"+filename); 
+		if (stream != null) rc.load(stream);
+		else throw new IOException("Couldn't load resource file '"+filename+"'. Make sure it exists and is accessible");
+		return rc;
+	}
+
 	/**
 	 * Default DAO factory constructor
 	 */
