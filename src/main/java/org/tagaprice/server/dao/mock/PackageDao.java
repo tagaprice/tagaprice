8e0392e653ec5dd1aad9630b310d52627503a470
Manuel Reithuber <git@manuel.reithuber.net>
2011-05-03 01:37:55 +0200

    mock-dao: PackageDao uses DaoClass now

--- src/main/java/org/tagaprice/server/dao/mock/PackageDao.java
+++ src/main/java/org/tagaprice/server/dao/mock/PackageDao.java
@@ -1,53 +1,11 @@
 package org.tagaprice.server.dao.mock;
 
-import java.util.HashMap;
 import java.util.List;
 
 import org.tagaprice.server.dao.IPackageDao;
-import org.tagaprice.server.rpc.ProductServiceImpl;
 import org.tagaprice.shared.entities.productmanagement.Package;
-import org.tagaprice.shared.logging.LoggerFactory;
-import org.tagaprice.shared.logging.MyLogger;
 
-public class PackageDao implements IPackageDao {
-	MyLogger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
-	HashMap<String, Package> packageAllRevisions = new HashMap<String, Package>();
-	int revIdCounter = 0;
-
-	@Override
-	public Package create(Package pkg) {
-		pkg.setId(new Integer(this.revIdCounter++).toString());
-		pkg.setRevision("1");
-		logger.log("create package. ID="+this.revIdCounter);
-		return pkg;
-	}
-
-	@Override
-	public Package get(String id, String revision) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Package get(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Package update(Package pkg) {
-		pkg.setRevision(pkg.getRevision() + 1);
-
-		packageAllRevisions.put(
-				pkg.getId(), pkg);
-		return pkg;
-	}
-
-	@Override
-	public void delete(Package pkg) {
-		// TODO Auto-generated method stub
-
-	}
+public class PackageDao extends DaoClass<Package> implements IPackageDao {
 
 	@Override
 	public List<Package> find(Package searchPattern) {
