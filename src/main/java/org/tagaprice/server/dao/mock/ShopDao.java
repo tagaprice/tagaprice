f3f3d21e3019d7db174d2a08498493ed92a52e3d
Manuel Reithuber <git@manuel.reithuber.net>
2011-05-03 01:52:28 +0200

    Mock-DAO: ShopDao extends DaoClass now

--- src/main/java/org/tagaprice/server/dao/mock/ShopDao.java
+++ src/main/java/org/tagaprice/server/dao/mock/ShopDao.java
@@ -1,6 +1,7 @@
 package org.tagaprice.server.dao.mock;
 
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -11,83 +12,22 @@ import org.tagaprice.shared.entities.shopmanagement.Shop;
 import org.tagaprice.shared.logging.LoggerFactory;
 import org.tagaprice.shared.logging.MyLogger;
 
-public class ShopDao implements IShopDao {
+public class ShopDao extends DaoClass<Shop> implements IShopDao {
 	MyLogger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
 
 	HashMap<String, Shop> shopsAllRevisions = new HashMap<String, Shop>();
 	HashMap<String, String> newestRev = new HashMap<String, String>();
 	Random random = new Random(1654196865);
 
-
 	@Override
-	public Shop create(Shop shop) {
-		logger.log("new Shop");
+	public List<Shop> list() {
+		ArrayList<Shop> rc = new ArrayList<Shop>();
 
-
-		shop.setId(new Long(random.nextLong()).toString());
-		shop.setRevision("1");
-		newestRev.put(shop.getId(), shop.getId());
-
-		shopsAllRevisions.put(shop.getId(), shop);
-
-		return shop;
-	}
-
-	@Override
-	public Shop get(String id, String revision) {
-		Shop rc = null;
-
-		//get id from
-		if(id!=null) {
-			if (revision == null) {
-				return shopsAllRevisions.get(newestRev.get(id));
-			}else{
-				return shopsAllRevisions.get(id);
-			}
+		for (Deque<Shop> deque: m_data.values()) {
+			rc.add(deque.peek());
 		}
 
 		return rc;
-	}
-
-	@Override
-	public Shop get(String id) {
-		return get(id, null);
-	}
-
-	@Override
-	public Shop update(Shop shop) {
-		logger.log("update shop");
-		//UPDATE
-		Shop updateShop = shopsAllRevisions.get(shop.getId());
-		if(updateShop == null){
-			//ERROR
-			logger.log("unexpected error");
-			return null;
-		}else{
-			updateShop=shop;
-
-			updateShop.setRevision(new Integer(Integer.parseInt(updateShop.getRevision())+1).toString());
-
-			shopsAllRevisions.put(updateShop.getId(), updateShop);
-			newestRev.put(updateShop.getId(), updateShop.getId());
-
-
-			return updateShop;
-		}
-	}
-
-	@Override
-	public void delete(Shop shop) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<Shop> list() {
-		ArrayList<Shop> list = new ArrayList<Shop>();
-		for(String s:shopsAllRevisions.keySet())
-			list.add(shopsAllRevisions.get(s));
-		return list;
 	}
 
 }
