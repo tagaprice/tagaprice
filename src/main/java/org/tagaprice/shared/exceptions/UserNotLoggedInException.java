01b700c416096478f6b49fa62f90a27af8aa7f79
Manuel Reithuber <git@manuel.reithuber.net>
2011-05-03 12:28:36 +0200

    added another Constructor to UserNotLoggedInException

--- src/main/java/org/tagaprice/shared/exceptions/UserNotLoggedInException.java
+++ src/main/java/org/tagaprice/shared/exceptions/UserNotLoggedInException.java
@@ -12,5 +12,9 @@ public class UserNotLoggedInException extends Exception {
 	public UserNotLoggedInException(String message) {
 		super(message);
 	}
+	
+	public UserNotLoggedInException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
