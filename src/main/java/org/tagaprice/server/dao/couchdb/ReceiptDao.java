4524a8e5c7c1c2d2b85007b72b369e3fa6373ce7
Jürgen Brandstetter <juergen@tagaprice.org>
2011-06-18 10:37:30 +0200

    removed get and setShopID from Receipt. It is never used and creates a parser error

--- src/main/java/org/tagaprice/server/dao/couchdb/ReceiptDao.java
+++ src/main/java/org/tagaprice/server/dao/couchdb/ReceiptDao.java
@@ -9,22 +9,26 @@ import org.tagaprice.server.dao.IReceiptDao;
 import org.tagaprice.shared.entities.receiptManagement.Receipt;
 import org.tagaprice.shared.exceptions.dao.DaoException;
 
+import com.allen_sauer.gwt.log.client.Log;
+
 public class ReceiptDao extends DaoClass<Receipt> implements IReceiptDao {
 	public ReceiptDao(CouchDbDaoFactory daoFactory) {
 		super(daoFactory, Receipt.class, "receipt", daoFactory._getEntityDao());
 	}
-	
+
 	@Override
 	public List<Receipt> list() throws DaoException {
+		Log.debug("List");
 		ViewResult<?> result = m_db.queryView("receipt/all", Receipt.class, null, null);
 		List<Receipt> rc = new ArrayList<Receipt>();
-		
-		System.out.println("CatList:");
+
+		Log.debug("Listsize: "+result.getRows().size());
 		for (ValueRow<?> row: result.getRows()) {
+			Log.debug("rowId: "+row.getId());
 			Receipt receipt = get(row.getId());
 			rc.add(receipt);
 		}
-		
+
 		return rc;
 	}
 
