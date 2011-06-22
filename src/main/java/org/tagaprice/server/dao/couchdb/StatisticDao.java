1f84f044489b79c9ee6e5074c4a4c90d29c0a2a6
Jürgen Brandstetter <juergen@tagaprice.org>
2011-06-22 11:27:55 +0200

    Add very simple statisticSearch methods but without efficient elasticsearch

--- src/main/java/org/tagaprice/server/dao/couchdb/StatisticDao.java
+++ src/main/java/org/tagaprice/server/dao/couchdb/StatisticDao.java
@@ -4,27 +4,93 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.tagaprice.server.dao.IReceiptDao;
 import org.tagaprice.server.dao.IStatisticDao;
 import org.tagaprice.shared.entities.BoundingBox;
+import org.tagaprice.shared.entities.receiptManagement.Receipt;
+import org.tagaprice.shared.entities.receiptManagement.ReceiptEntry;
 import org.tagaprice.shared.entities.searchmanagement.StatisticResult;
+import org.tagaprice.shared.exceptions.dao.DaoException;
 
 public class StatisticDao extends DaoClass<StatisticResult> implements IStatisticDao {
 
+	private IReceiptDao m_receiptDao;
 
 	public StatisticDao(CouchDbDaoFactory daoFactory) {
 		super(daoFactory, StatisticResult.class, "statistic", daoFactory._getEntityDao());
+		m_receiptDao = daoFactory.getReceiptDao();
 	}
 
 	@Override
 	public ArrayList<StatisticResult> searchPricesViaProduct(String productId, BoundingBox bbox, Date begin, Date end) {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO Search efficient via db or elasticsearch
+		//Test Data
+		ArrayList<StatisticResult> rc = new ArrayList<StatisticResult>();
+
+		try {
+			for(Receipt r:m_receiptDao.list()){
+				if(r.getShop().getAddress().getLat()<bbox.getNorthEastLat() &&
+						r.getShop().getAddress().getLat()>bbox.getSouthWestLat() &&
+						r.getShop().getAddress().getLng()<bbox.getNorthEastLng() &&
+						r.getShop().getAddress().getLng()>bbox.getSouthWestLng()){
+					for(ReceiptEntry re:r.getReceiptEntries()){
+						if(productId.equals(re.getPackage().getProduct().getId())){
+							rc.add(new StatisticResult(
+									r.getDate(),
+									r.getShop(),
+									null,
+									re.getPackage().getQuantity(),
+									re.getPrice()));
+						}
+
+					}
+				}
+
+			}
+		} catch (DaoException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return rc;
 	}
 
 	@Override
 	public List<StatisticResult> searchPricesViaShop(String shopId, BoundingBox bbox, Date begin, Date end) {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO Search efficient via db or elasticsearch
+		//Test Data
+		ArrayList<StatisticResult> rc = new ArrayList<StatisticResult>();
+
+		try {
+			for(Receipt r:m_receiptDao.list()){
+				if(r.getShop().getAddress().getLat()<bbox.getNorthEastLat() &&
+						r.getShop().getAddress().getLat()>bbox.getSouthWestLat() &&
+						r.getShop().getAddress().getLng()<bbox.getNorthEastLng() &&
+						r.getShop().getAddress().getLng()>bbox.getSouthWestLng()){
+
+					if(shopId.equals(r.getShop().getId())){
+						for(ReceiptEntry re:r.getReceiptEntries()){
+							rc.add(new StatisticResult(
+									r.getDate(),
+									null,
+									re.getPackage().getProduct(),
+									re.getPackage().getQuantity(),
+									re.getPrice()));
+						}
+
+
+
+
+					}
+				}
+
+			}
+		} catch (DaoException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return rc;
 	}
 
 
