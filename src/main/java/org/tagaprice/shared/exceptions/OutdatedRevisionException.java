44dfb905fd6646698840e28f0012b56bdbd81abe
Jürgen Brandstetter <juergen@tagaprice.org>
2011-08-31 17:26:14 +0200

    add IsSerializeable to WrongEmailOrPasswordException

--- src/main/java/org/tagaprice/shared/exceptions/OutdatedRevisionException.java
+++ src/main/java/org/tagaprice/shared/exceptions/OutdatedRevisionException.java
@@ -1,14 +1,16 @@
 package org.tagaprice.shared.exceptions;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
 
 /**
  * Thrown to indicate that at least one of the revisions of a given product is outdated, i.e. another revision of the product has been saved already.
  * @author "forste"
  *
  */
-public class OutdatedRevisionException extends Exception {
+public class OutdatedRevisionException extends Exception implements IsSerializable{
 
-	private static final long serialVersionUID = 2814657980704339544L;
+	private static final long serialVersionUID = 1L;
 
 	public OutdatedRevisionException(String message) {
 		super(message);
--- src/main/java/org/tagaprice/shared/exceptions/UserAlreadyLoggedInException.java
+++ src/main/java/org/tagaprice/shared/exceptions/UserAlreadyLoggedInException.java
@@ -1,7 +1,9 @@
 package org.tagaprice.shared.exceptions;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class UserAlreadyLoggedInException extends Exception {
+
+public class UserAlreadyLoggedInException extends Exception implements IsSerializable {
 
 	private static final long serialVersionUID = 1L;
 
