ae8d7f41133f365bb08eab81b6f869160776ab71
Jürgen Brandstetter <juergen@tagaprice.org>
2011-09-01 09:38:10 +0200

    removed unused code in unitSelecter

--- src/main/java/org/tagaprice/client/generics/widgets/desktopView/UnitSelecter.java
+++ src/main/java/org/tagaprice/client/generics/widgets/desktopView/UnitSelecter.java
@@ -15,7 +15,6 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -24,11 +23,9 @@ public class UnitSelecter extends Composite implements IUnitSelecter{
 	private Label _selectButton = new Label("Unit");
 	private PopupPanel _showUnits = new PopupPanel(true);
 	private Unit _cUnit = new Unit();
-	private ListBox _listBoxUnit = new ListBox();
 	private IUnitChangedHandler _unitChangedHandler;
 	private IUnitServiceAsync _unitServiceAsync = GWT.create(IUnitService.class);
 	private String _allowId = null;
-	private VerticalPanel _unitList = new VerticalPanel();
 
 	private boolean _readonly = true;
 	private boolean _isHeadline = false;
@@ -37,11 +34,11 @@ public class UnitSelecter extends Composite implements IUnitSelecter{
 		
 		_selectButton.setStyleName("unitSelecter");
 		_showUnits.setStyleName("popBackground");
+		_showUnits.getElement().getStyle().setZIndex(2000);
 		
 		initWidget(_selectButton);
 		setRelatedUnit(null);
-		_showUnits.setWidget(_unitList);
-		
+
 		_selectButton.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -55,19 +52,11 @@ public class UnitSelecter extends Composite implements IUnitSelecter{
 		});
 
 
-		//add Listener
-		_listBoxUnit.addChangeHandler(new ChangeHandler() {
-
-			@Override
-			public void onChange(ChangeEvent arg0) {
-				if(_unitChangedHandler!=null)_unitChangedHandler.onChange(getUnit());
-
-			}
-		});
 	}
 
 	@Override
 	public void setRelatedUnit(Unit unit) {
+		Log.debug("RelativeUnit: "+unit);
 		if(unit!=null){
 			if(unit.getParent()==null)
 				_allowId=unit.getId();
@@ -79,12 +68,10 @@ public class UnitSelecter extends Composite implements IUnitSelecter{
 	@Override
 	public void setUnit(Unit unit) {
 		if(unit!=null){
-			Log.debug("setUnit: "+unit.getTitle());
 			_selectButton.setText(unit.getTitle());
 			_cUnit=unit;
 			int pos = 0;
 
-			_listBoxUnit.setSelectedIndex(pos);
 		}else{
 			_cUnit=new Unit();
 			_selectButton.setText("Unit");
@@ -94,7 +81,6 @@ public class UnitSelecter extends Composite implements IUnitSelecter{
 	@Override
 	public Unit getUnit() {
 		return _cUnit;
-		//return Unit.valueOf(_listBoxUnit.getItemText(_listBoxUnit.getSelectedIndex()));
 	}
 
 
@@ -106,43 +92,40 @@ public class UnitSelecter extends Composite implements IUnitSelecter{
 
 	private void showFactorizedUnits(){
 
-		if(_unitList.getWidgetCount()>0){
-			_showUnits.setWidget(_unitList);
-			_showUnits.showRelativeTo(_selectButton);
-		}else{
-			_unitServiceAsync.getFactorizedUnits(_allowId, new AsyncCallback<List<Unit>>() {
-	
-				@Override
-				public void onSuccess(List<Unit> results) {
-	
-	
-	
-					for(final Unit u:results){
-						Label l = new Label(u.getTitle());
-						_unitList.add(l);
-	
-						l.addClickHandler(new ClickHandler() {
-	
-							@Override
-							public void onClick(ClickEvent arg0) {
-								setUnit(u);
-								_showUnits.hide();
-								if(_unitChangedHandler!=null)_unitChangedHandler.onChange(getUnit());
-							}
-						});
-					}
-	
-					_showUnits.setWidget(_unitList);
-					_showUnits.showRelativeTo(_selectButton);
-	
+		_unitServiceAsync.getFactorizedUnits(_allowId, new AsyncCallback<List<Unit>>() {
+
+			@Override
+			public void onSuccess(List<Unit> results) {
+				Log.debug("get factorizedUnits successfull. count: "+results.size());
+				VerticalPanel vePa = new VerticalPanel();
+
+
+
+				for(final Unit u:results){
+					Label l = new Label(u.getTitle());
+					vePa.add(l);
+
+					l.addClickHandler(new ClickHandler() {
+
+						@Override
+						public void onClick(ClickEvent arg0) {
+							setUnit(u);
+							_showUnits.hide();
+							if(_unitChangedHandler!=null)_unitChangedHandler.onChange(getUnit());
+						}
+					});
 				}
-	
-				@Override
-				public void onFailure(Throwable e) {
-					Log.error("showFactorizedUnitsProblem: "+e);
-				}
-			});
-		}
+
+				_showUnits.setWidget(vePa);
+				_showUnits.showRelativeTo(_selectButton);
+
+			}
+
+			@Override
+			public void onFailure(Throwable e) {
+				Log.error("showFactorizedUnitsProblem: "+e);
+			}
+		});
 
 
 	}
