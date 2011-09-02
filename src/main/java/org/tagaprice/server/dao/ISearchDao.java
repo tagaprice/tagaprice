dec958e665356eeaf50a44ed8098313489f71392
Manuel Reithuber <git@manuel.reithuber.net>
2011-09-02 13:08:45 +0200

    added searchProduct() and searchShop() to ISearchDao

--- src/main/java/org/tagaprice/server/dao/ISearchDao.java
+++ src/main/java/org/tagaprice/server/dao/ISearchDao.java
@@ -8,4 +8,6 @@ import org.tagaprice.shared.exceptions.dao.DaoException;
 
 public interface ISearchDao {
 	public List<Document> search(String query, BoundingBox bbox, int limit) throws DaoException;
+	public List<Document> searchProduct(String query, int limit) throws DaoException;
+	public List<Document> searchShop(String query, BoundingBox bbox, int limit) throws DaoException;
 }
