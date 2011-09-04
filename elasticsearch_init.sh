6532360a4a7c9aaed2362506982977a05afe4b7f
Manuel Reithuber <git@manuel.reithuber.net>
2011-09-04 22:22:50 +0200

    updated elasticsearch_init.sh to support the new index.json

--- elasticsearch_init.sh
+++ elasticsearch_init.sh
@@ -4,14 +4,15 @@ couchHost=localhost
 couchPort=5984
 couchDb=tagaprice
 
+if [ -n "$1" ]; then
+	baseUrl="$1"
+fi
+
 riverData=`cat src/main/webapp/WEB-INF/classes/elasticsearch/river.json | sed "s/{COUCH_HOST}/$couchHost/" | sed "s/{COUCH_PORT}/$couchPort/" | sed "s/{COUCH_DB}/$couchDb/"`
 
-echo "Creating index..."
-curl -XPUT "$baseUrl/tagaprice"
-echo
-echo "Setting mapping..."
-curl -XPUT "$baseUrl/tagaprice/tagaprice/_mapping" -d@src/main/webapp/WEB-INF/classes/elasticsearch/mapping.json
+echo "Creating index with mappings..."
+curl -XPUT "$baseUrl/tagaprice" -d@src/main/webapp/WEB-INF/classes/elasticsearch/index.json || exit $?
 echo
 echo "Creating river..."
-curl -XPUT "$baseUrl/_river/tagaprice/_meta" "-d$riverData"
+curl -XPUT "$baseUrl/_river/tagaprice/_meta" "-d$riverData" || exit $?
 echo
