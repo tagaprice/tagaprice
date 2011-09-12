38a9dfe33299f81f2c4b08e068b5571745b7037d
Manuel Reithuber <git@manuel.reithuber.net>
2011-09-12 23:58:06 +0200

    added missing DummyServiceImpl

---
+++ src/main/java/org/tagaprice/server/rpc/DummyServiceImpl.java
@@ -0,0 +1,21 @@
+package org.tagaprice.server.rpc;
+
+import java.util.HashMap;
+
+import org.tagaprice.shared.entities.receiptManagement.ReceiptEntry;
+import org.tagaprice.shared.rpc.DummyService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class DummyServiceImpl extends RemoteServiceServlet implements DummyService {
+	@Override
+	public void dummyMethode(Boolean bool, boolean bool2,
+			ReceiptEntry eceiptEntry, HashMap<String, String> dummyMap) {
+		// TODO Auto-generated method stub
+		
+	}
+}
