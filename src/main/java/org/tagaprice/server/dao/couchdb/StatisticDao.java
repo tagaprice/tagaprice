e3d00cb0b766fe8d65c9d212960e45c9aa98080c
Manuel Reithuber <git@manuel.reithuber.net>
2011-09-13 11:18:08 +0200

    trying to speed up searchPricesViaProduct() by using DaoClass.getOnly() and by caching the requested objects

--- src/main/java/org/tagaprice/server/dao/couchdb/StatisticDao.java
+++ src/main/java/org/tagaprice/server/dao/couchdb/StatisticDao.java
@@ -2,9 +2,13 @@ package org.tagaprice.server.dao.couchdb;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.tagaprice.server.dao.IDaoClass;
 import org.tagaprice.server.dao.IPackageDao;
+import org.tagaprice.server.dao.IProductDao;
 import org.tagaprice.server.dao.IReceiptDao;
 import org.tagaprice.server.dao.IShopDao;
 import org.tagaprice.server.dao.IStatisticDao;
@@ -18,24 +22,26 @@ import org.tagaprice.server.dao.couchdb.elasticsearch.query.Terms;
 import org.tagaprice.server.dao.couchdb.elasticsearch.result.Hit;
 import org.tagaprice.server.dao.couchdb.elasticsearch.result.SearchResult;
 import org.tagaprice.shared.entities.BoundingBox;
+import org.tagaprice.shared.entities.Document;
 import org.tagaprice.shared.entities.productmanagement.Package;
+import org.tagaprice.shared.entities.productmanagement.Product;
 import org.tagaprice.shared.entities.receiptManagement.Receipt;
 import org.tagaprice.shared.entities.receiptManagement.ReceiptEntry;
 import org.tagaprice.shared.entities.searchmanagement.StatisticResult;
 import org.tagaprice.shared.entities.shopmanagement.Shop;
 import org.tagaprice.shared.exceptions.dao.DaoException;
 
-import com.allen_sauer.gwt.log.client.Log;
-
 public class StatisticDao extends DaoClass<StatisticResult> implements IStatisticDao {
 	private ElasticSearchClient m_searchClient;
 	private IPackageDao m_packageDao;
+	private IProductDao m_productDao;
 	private IReceiptDao m_receiptDao;
 	private IShopDao m_shopDao;
 
 	public StatisticDao(CouchDbDaoFactory daoFactory) {
 		super(daoFactory, StatisticResult.class, "statistic", daoFactory._getDocumentDao());
 		m_packageDao = daoFactory.getPackageDao();
+		m_productDao = daoFactory.getProductDao();
 		m_receiptDao = daoFactory.getReceiptDao();
 		m_shopDao = daoFactory.getShopDao();
 		m_searchClient = daoFactory.getElasticSearchClient();
@@ -43,15 +49,14 @@ public class StatisticDao extends DaoClass<StatisticResult> implements IStatisti
 
 	@Override
 	public List<StatisticResult> searchPricesViaProduct(String productId, BoundingBox bbox, Date begin, Date end) throws DaoException {
+		List<StatisticResult> rc = new ArrayList<StatisticResult>();
+		
 		// get all the packages of the product in question
 		List<String> packageIDs = m_packageDao.listIDsByProduct(productId);
 		// get all the shops in the specified bbox
 		List<String> shopIDs = m_shopDao.findIDsInBBox(bbox);
-		
-	
-		List<StatisticResult> rc = new ArrayList<StatisticResult>();
-		
-		if(shopIDs.size()>0){
+
+		if (shopIDs.size() > 0) {
 			// find all receipts that contain at least one of the packages AND at least one of the shops
 			QueryObject queryObject = new QueryObject().query(
 				new FilteredQuery().query(
@@ -67,18 +72,27 @@ public class StatisticDao extends DaoClass<StatisticResult> implements IStatisti
 	
 			SearchResult searchResult = m_searchClient.find(queryObject);
 			
+			// cache the results to avoid fetching too much data
+			Map<String, Package> packageCache = new HashMap<String, Package>();
+			Map<String, Product> productCache = new HashMap<String, Product>();
+			Map<String, Shop> shopCache = new HashMap<String, Shop>();
+			
 			for (Hit hit: searchResult.getHits().getHits()) {
-				Receipt receipt = m_receiptDao.get(hit.getId());
-				Shop shop = receipt.getShop();
+				Receipt receipt = m_receiptDao.getOnly(hit.getId());
+				Shop shop = _getCached(m_shopDao, receipt.getShopId(), shopCache);
 				for (ReceiptEntry entry: receipt.getReceiptEntries()) {
 					if (packageIDs.contains(entry.getPackageId())) {
-						Package pkg = entry.getPackage();
-						rc.add(new StatisticResult(receipt.getDate(), shop, pkg.getProduct(), pkg.getQuantity(), entry.getPrice()));
+						Package pkg = _getCached(m_packageDao, entry.getPackageId(), packageCache);
+						rc.add(new StatisticResult(receipt.getDate(), shop, _getCached(m_productDao, pkg.getProductId(), productCache), pkg.getQuantity(), entry.getPrice()));
 					}
 				}
 			}
+			
+			// make sure those don't stay in the server's memory for too long
+			packageCache.clear();
+			productCache.clear();
+			shopCache.clear();
 		}
-		Log.debug("got "+rc.size()+" results");
 		
 		return rc;
 	}
@@ -100,6 +114,12 @@ public class StatisticDao extends DaoClass<StatisticResult> implements IStatisti
 		return rc;
 	}
 
+	private static <T extends Document> T _getCached(IDaoClass<T> daoClass, String id, Map<String, T> cache) throws DaoException {
+		if (!cache.containsKey(id)) {
+			cache.put(id, daoClass.getOnly(id));
+		}
+		return cache.get(id);
+	}
 
 
 
