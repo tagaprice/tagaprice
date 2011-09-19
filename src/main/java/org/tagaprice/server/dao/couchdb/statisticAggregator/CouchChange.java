95ccb6e112c6bd246b074d763a09a6b9a8c2bf2a
Manuel Reithuber <git@manuel.reithuber.net>
2011-09-19 14:48:57 +0200

    added class StatisticAggregator (not tested yet)

---
+++ src/main/java/org/tagaprice/server/dao/couchdb/statisticAggregator/CouchChange.java
@@ -0,0 +1,77 @@
+package org.tagaprice.server.dao.couchdb.statisticAggregator;
+
+import java.util.Set;
+
+import org.svenson.DynamicProperties;
+import org.tagaprice.shared.entities.Document;
+
+public class CouchChange implements DynamicProperties {
+	private long _seq;
+	private String _id;
+	private boolean _deleted = false;
+	private Document _document;
+	
+	public CouchChange() {}
+	
+	//
+	// Getters
+	//
+	public Document getDoc() {
+		return _document;
+	}
+	
+	public String getId() {
+		return _id;
+	}
+	
+	public long getSeq() {
+		return _seq;
+	}
+	
+	public boolean getDeleted() {
+		return _deleted;
+	}
+	
+	
+	//
+	// Setters
+	//
+	/*public void setChanges(Object changes) {
+		// do nothing
+	}*/
+	
+	public void setDeleted(boolean deleted) {
+		_deleted = deleted;
+	}
+	
+	public void setDoc(Document document) {
+		_document = document;
+	}
+	
+	public void setId(String id) {
+		_id = id;
+	}
+	
+	public void setSeq(long seq) {
+		_seq = seq;
+	}
+
+	@Override
+	public void setProperty(String name, Object value) {
+		System.out.println("::setProperty('"+name+"', '"+value.toString()+"')");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getProperty(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> propertyNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
